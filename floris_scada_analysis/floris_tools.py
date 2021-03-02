0ff1a7a6b4adc856a552b233aa6ccd9ea007ad25
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-03-01 18:51:08 -0700

    Add a function that automatically calculates which turbines are upstream and for which WD bins

--- floris_scada_analysis/floris_tools.py
+++ floris_scada_analysis/floris_tools.py
@@ -41,6 +41,59 @@ def _get_turbine_cutout_ws(fCpInterp):
 
 
 # Define an approximate calc_floris() function
+def calc_floris(df, fi):
+    """Calculate the FLORIS predictions for a particular wind direction, wind speed
+    and turbulence intensity set. This function calculates the exact solutions.
+
+    Args:
+        df ([pd.DataFrame]): Dataframe with at least the columns 'time', 'wd' 
+        and 'ws'. Can optionally also have the column 'ti' and 'time'. Any
+        other column will be ignored.
+        fi ([FlorisInterface]): Floris object for the wind farm of interest
+
+    Returns:
+        [type]: [description]
+    """
+
+    num_turbines = len(fi.layout_x)
+
+    # Start by ensuring simple index for df
+    df = df.reset_index(drop=('time' in df.columns))
+
+    # Generate new dataframe called df_out
+    if not ('ti' in df.columns):
+        df['ti'] = np.min(fi.floris.farm.turbulence_intensity)
+
+    if 'time' in df.columns:
+        df_out = df[['time', 'wd', 'ws', 'ti']]
+    else:
+        df_out = df[['wd', 'ws', 'ti']]
+
+    # Create placeholders for turbine measurements
+    for colname in ['pow', 'ws', 'wd', 'ti']:
+        for ti in range(num_turbines):
+            df_out[colname + '_%03d' % ti] = 0.0
+
+    for idx in range(df_out.shape[0]):
+        if (np.remainder(idx, 100) == 0) or idx == df_out.shape[0]-1:  # Print output every 100 steps:
+            print('  Progress: finished %.1f percent (%d/%d cases).'
+                  % (100.*idx/df_out.shape[0], idx, df_out.shape[0]))
+
+        fi.reinitialize_flow_field(wind_speed=df_out.loc[idx, 'ws'],
+                                   wind_direction=df_out.loc[idx, 'wd'],
+                                   turbulence_intensity=df_out.loc[idx, 'ti'])
+        fi.calculate_wake()
+
+        for ti in range(num_turbines):
+            df_out.loc[idx, 'pow_%03d' % ti] = np.array(fi.get_turbine_power())[ti]/1000.
+            df_out.loc[idx, 'wd_%03d' % ti] = df_out.loc[idx, 'wd']  # Assume uniform for now
+            df_out.loc[idx, 'ws_%03d' % ti] = fi.floris.farm.turbines[ti].average_velocity
+            df_out.loc[idx, 'ti_%03d' % ti] = fi.floris.farm.turbines[ti]._turbulence_intensity
+
+    print('Finished calculating the FLORIS solutions for the dataframe.')
+    return df_out
+
+
 def calc_floris_approx(df, fi, ws_step=0.5, wd_step=1.0, ti_step=None, method='linear'):
     """Calculate the FLORIS predictions for a particular wind direction, wind speed
     and turbulence intensity set. This function approximates the exact solutions
@@ -78,7 +131,8 @@ def calc_floris_approx(df, fi, ws_step=0.5, wd_step=1.0, ti_step=None, method='l
     if 'ti' in df.columns:
         ti_array = df['ti']
     else:
-        ti_array = np.repeat(0.06, df.shape[0])
+        ti_fi = np.min(fi.floris.farm.turbulence_intensity)
+        ti_array = np.repeat(ti_fi, df.shape[0])
 
     wd_min = np.max([np.min(wd_array), 0.0])
     wd_max = np.min([np.max(wd_array), 360.0])
@@ -107,37 +161,35 @@ def calc_floris_approx(df, fi, ws_step=0.5, wd_step=1.0, ti_step=None, method='l
          'ws': np.reshape(xyz_grid[1], [-1, 1]).flatten(),
          'ti': np.reshape(xyz_grid[2], [-1, 1]).flatten()})
 
-    for colname in ['pow', 'ws', 'wd', 'ti']:
-        for ti in range(num_turbines):
-            df_approx[colname + '_%03d' % ti] = 0.0
-
     if df.shape[0] <= df_approx.shape[0]:
         print('Approximation would not reduce number of cases with the current settings (N_raw=' +str(N_raw)+', N_approx='+str(N_approx)+')')
         print('Calculating the exact solutions for this dataset. Avoiding any approximations.')
-        breakpoint()
-        # return calc_floris()
+        return calc_floris(df, fi)
 
+    # Calculate approximate solutions
     print('Reducing calculations from ' + str(df.shape[0]) + ' to ' + str(df_approx.shape[0]) + ' cases.')
-    for idx in range(df_approx.shape[0]):
-        if (np.remainder(idx, 100) == 0) or idx == df_approx.shape[0]-1:  # Print output every 100 steps:
-            print('  Progress: finished %.1f percent (%d/%d cases).'
-                  % (100.*idx/df_approx.shape[0], idx, df_approx.shape[0]))
+    df_approx = calc_floris(df_approx, fi)
 
-        fi.reinitialize_flow_field(wind_speed=df_approx.loc[idx, 'ws'],
-                                   wind_direction=df_approx.loc[idx, 'wd'],
-                                   turbulence_intensity=df_approx.loc[idx, 'ti'])
-        fi.calculate_wake()
+    # for idx in range(df_approx.shape[0]):
+    #     if (np.remainder(idx, 100) == 0) or idx == df_approx.shape[0]-1:  # Print output every 100 steps:
+    #         print('  Progress: finished %.1f percent (%d/%d cases).'
+    #               % (100.*idx/df_approx.shape[0], idx, df_approx.shape[0]))
 
-        for ti in range(num_turbines):
-            df_approx.loc[idx, 'pow_%03d' % ti] = np.array(fi.get_turbine_power())[ti]/1000.
-            df_approx.loc[idx, 'wd_%03d' % ti] = df_approx.loc[idx, 'wd']  # Assume uniform for now
-            df_approx.loc[idx, 'ws_%03d' % ti] = fi.floris.farm.turbines[ti].average_velocity
-            df_approx.loc[idx, 'ti_%03d' % ti] = fi.floris.farm.turbines[ti]._turbulence_intensity
+    #     fi.reinitialize_flow_field(wind_speed=df_approx.loc[idx, 'ws'],
+    #                                wind_direction=df_approx.loc[idx, 'wd'],
+    #                                turbulence_intensity=df_approx.loc[idx, 'ti'])
+    #     fi.calculate_wake()
+
+    #     for ti in range(num_turbines):
+    #         df_approx.loc[idx, 'pow_%03d' % ti] = np.array(fi.get_turbine_power())[ti]/1000.
+    #         df_approx.loc[idx, 'wd_%03d' % ti] = df_approx.loc[idx, 'wd']  # Assume uniform for now
+    #         df_approx.loc[idx, 'ws_%03d' % ti] = fi.floris.farm.turbines[ti].average_velocity
+    #         df_approx.loc[idx, 'ti_%03d' % ti] = fi.floris.farm.turbines[ti]._turbulence_intensity
 
     # Map individual data entries to full DataFrame
     print('Now mapping the precalculated solutions from FLORIS to the dataframe entries...')
     print("  Creating a gridded interpolant with interpolation method '" + method + "'.")
-    
+
     # Create interpolant
     if len(ti_array_approx) <= 1:
         print('    Performing 2D interpolation')
@@ -147,7 +199,6 @@ def calc_floris_approx(df, fi, ws_step=0.5, wd_step=1.0, ti_step=None, method='l
     else:
         print('    Performing 3D interpolation')
         shape_y = [len(wd_array_approx), len(ws_array_approx), len(ti_array_approx)]
-        # values = np.reshape(np.array(df_approx['pow_000']), shape_y)
         values = np.reshape(np.array(df_approx['pow_000']), shape_y)
         xyz_tuple = (wd_array_approx, ws_array_approx, ti_array_approx)
     f = interpolate.RegularGridInterpolator(xyz_tuple, values,
@@ -179,3 +230,50 @@ def calc_floris_approx(df, fi, ws_step=0.5, wd_step=1.0, ti_step=None, method='l
 
     print('Finished calculating the FLORIS solutions for the dataframe.')
     return df_out
+
+
+def get_upstream_turbs_floris(fi, wd_step=1.0):
+    print('Determining upstream turbines using FLORIS for wd_step = %.1f deg.' %(wd_step))
+    upstream_turbs_ids = []  # turbine numbers that are freestream
+    upstream_turbs_wds = []  # lower bound of bin
+    for wd in np.arange(0., 360., wd_step):
+        fi.reinitialize_flow_field(wind_direction=wd, wind_speed=8.0)
+        fi.calculate_wake()
+        power_out = np.array(fi.get_turbine_power())
+        power_wake_loss = np.max(power_out) - power_out
+        turbs_freestream = list(np.where(power_wake_loss < 0.01)[0])
+        if len(upstream_turbs_wds) == 0:
+            upstream_turbs_ids.append(turbs_freestream)
+            upstream_turbs_wds.append(wd)
+        elif not(turbs_freestream == upstream_turbs_ids[-1]):
+            upstream_turbs_ids.append(turbs_freestream)
+            upstream_turbs_wds.append(wd)
+
+    # Connect at 360 degrees
+    if upstream_turbs_ids[0] == upstream_turbs_ids[-1]:
+        upstream_turbs_wds.pop(0)
+        upstream_turbs_ids.pop(0)
+
+    # Go from list to bins for upstream_turbs_wds
+    upstream_turbs_wds = [[upstream_turbs_wds[i], upstream_turbs_wds[i+1]] for i in range(len(upstream_turbs_wds)-1)]
+    upstream_turbs_wds.append([upstream_turbs_wds[-1][-1], upstream_turbs_wds[0][0]])
+
+    df_upstream = pd.DataFrame({'wd_min': [wd[0] for wd in upstream_turbs_wds],
+                                'wd_max': [wd[1] for wd in upstream_turbs_wds],
+                                'turbines': upstream_turbs_ids})
+
+    return df_upstream
+
+
+if __name__ == '__main__':
+    import os
+    import floris.tools as wfct
+    
+    # Initialize the FLORIS interface fi
+    print('Initializing the FLORIS object for our demo wind farm')
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    fi_path = os.path.join(file_path, "../examples/demo_dataset/demo_floris_input.json")
+    fi = wfct.floris_interface.FlorisInterface(fi_path)
+
+    df_upstream = get_upstream_turbs_floris(fi)
+    print(df_upstream)
