8503f738146043c4c3b297e954acc543b171bdd8
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-05-01 09:43:41 -0600

    Fairly big change in bias_estimation since there was an inherent problem in there. Namely, previously, I would specify the reference wind speed and power measurements BEFORE estimating the wind direction bias. Now, if the pow_ref and ws are based on the upstream turbines, then inherently we need the wind direction to figure out what the upstream turbines are. That means we actually need to change pow_ref (and ws) as our hypothesis of the true wind direction changes. Hence, I am now including functions for pow_ref and ws into the bias_estimation class. These are functions that just take the dataframe and output a dataframe with an addtional column called pow_ref or ws, respectively. These changes also mean that we recalculate the energy ratios for every hypothesized wind direction bias. This significantly slows down the code, but now the wind direction bias estimations are much more accurate and the code becomes more transparent. We are no longer shifting the curves along the x-axis to get a good match (even though if we choose a wind-direction-independent pow_ref/ws, essentially it would end up to the same outcome). It makes sense to start evaluating these things on Eagle rather than on a local computer, especially for larger farms/datasets and if you would like to get a WD correction for every turbine individually.

--- floris_scada_analysis/bias_estimation.py
+++ floris_scada_analysis/bias_estimation.py
@@ -12,14 +12,17 @@
 
 
 import datetime
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-from pandas.core.base import DataError
-
+# from pandas.core.base import DataError
+from scipy import optimize as opt
+from scipy import stats as spst
 from floris.utilities import wrap_360
 
+from floris_scada_analysis import dataframe_manipulations as dfm
 from floris_scada_analysis import floris_tools as ftools
-from floris_scada_analysis import optimization as opt
+# from floris_scada_analysis import optimization as fopt
 from floris_scada_analysis import scada_analysis as sca
 from floris_scada_analysis import time_operations as tops
 
@@ -31,42 +34,52 @@ def printnow(text):
 
 
 class bias_estimation():
-    def __init__(self, df, df_fi, fi, test_turbines_subset,
-                 sliding_window_lb_ub, df_upstream=None,
-                 eo_ws_step=5.0, eo_wd_step=2.0, verbose=False):
+    def __init__(self, df, fi, test_turbines_subset, sliding_window_lb_ub,
+                 df_ws_mapping_func, df_pow_ref_mapping_func,
+                 df_upstream=None, df_fi_approx=None,  eo_ws_step=5.0,
+                 eo_wd_step=2.0, df_ws_range=None, verbose=False):
         printnow('Initializing the bias_correction() class from floris_scada_analysis.')
 
         self.verbose = verbose
 
-        # Check format of dataframes based on first 100 rows
-        if df.shape[0] != df_fi.shape[0]:
-            raise DataError('Please ensure df_fi and df have the same number of entries.')
-        if not (list(df.iloc[0:100]['time'])==list(df_fi.iloc[0:100]['time'])):
-            raise DataError("The columns 'time' should be identical between df and df_fi.")
-        if not (list(df.iloc[0:100]['wd'])==list(df_fi.iloc[0:100]['wd'])):
-            raise DataError("The columns 'wd' should be identical between df and df_fi.")
-        if not (list(df.iloc[0:100]['ws'])==list(df_fi.iloc[0:100]['ws'])):
-            raise DataError("The columns 'ws' should be identical between df and df_fi.")
+        # # Check format of dataframes based on first 100 rows
+        # if df.shape[0] != df_fi.shape[0]:
+        #     raise DataError('Please ensure df_fi and df have the same number of entries.')
+        # if not (list(df.iloc[0:100]['time'])==list(df_fi.iloc[0:100]['time'])):
+        #     raise DataError("The columns 'time' should be identical between df and df_fi.")
+        # if not (list(df.iloc[0:100]['wd'])==list(df_fi.iloc[0:100]['wd'])):
+        #     raise DataError("The columns 'wd' should be identical between df and df_fi.")
+        # if not (list(df.iloc[0:100]['ws'])==list(df_fi.iloc[0:100]['ws'])):
+        #     raise DataError("The columns 'ws' should be identical between df and df_fi.")
 
         df = df.reset_index(drop=('time' in df.columns))
-        df_fi = df_fi.reset_index(drop=('time' in df_fi.columns))
+        # df_fi = df_fi.reset_index(drop=('time' in df_fi.columns))
 
-        # Import
+        # Import inputs
         self.fi = fi  # Floris object
         self.df = df  # Actual measurements
-        self.df_fi = df_fi  # Floris predictions
+        if df_fi_approx is None:
+            df_fi = df[['time', 'ws', 'wd']].copy()
+            _, df_fi_approx = ftools.calc_floris_approx(
+                df_fi, fi, wd_step=2.0, num_threads=40)
+        self.df_fi_approx = df_fi_approx  # Save FLORIS solutions LUT
         self.df_subset = None # Subset of interest (actual)
-        self.df_fi_subset = None # Subset of interest (floris)
+
+        self.df_ws_mapping_func = df_ws_mapping_func
+        self.df_pow_ref_mapping_func = df_pow_ref_mapping_func
+        # self.df_fi = df_fi  # Floris predictions
+        # self.df_fi_subset = None # Subset of interest (floris)
 
         self.turbine_list = range(len(fi.layout_x))
         self.test_turbines_subset = test_turbines_subset
         # self.ref_turbine_maxrange = ref_turbine_maxrange
         self.eo_wd_step = eo_wd_step
         self.eo_ws_step = eo_ws_step
+        if df_ws_range is None:
+            df_ws_range = (0., 50.)
+        self.df_ws_range = df_ws_range
 
         self._reset_results()
-        self.opt_search_range = (-180., 180.)
-        self.opt_search_dx = 0.50
 
         self.df_upstream = df_upstream
         self._map_ref_turbs_floris()
@@ -94,50 +107,10 @@ class bias_estimation():
         self._reset_results()
         self._update_df_subset()
 
-    def estimate_wd_bias(self, N_btstrp=1):
-        printnow('Estimating the wind direction bias')
-        self._get_energy_ratios_allbins(N_btstrp=N_btstrp)
-
-        # printnow('  Combining %d turbine energy ratios into single curve'
-        #          % len(self.test_turbines_subset))
-        # x_array = []
-        # y_scada = []
-        # y_floris = []
-        # for ii in range(len(self.test_turbines_subset)):
-        #     x_array.extend(ii*400. + np.array(self.energy_ratio_wd_total[ii]))  # Space curves apart by > 360.
-        #     y_scada.extend(self.energy_ratio_scada_total[ii])
-        #     y_floris.extend(self.energy_ratio_floris_total[ii])
-
-        # printnow('  Matching SCADA and FLORIS energy ratio curves...')
-        # wd_bias, success = opt.find_bias_x(x_array, y_scada, x_array, y_floris,
-        #                                    search_range = self.opt_search_range,
-        #                                    search_dx = self.opt_search_dx)
-
-        # Unpack variables
-        wd_arrays = [self.energy_ratios_scada[i]['wd_bin'] for i
-                     in range(len(self.energy_ratios_scada))]
-        er_result_scada = [self.energy_ratios_scada[i]['baseline'] for i
-                           in range(len(self.energy_ratios_scada))]
-        er_result_floris = [self.energy_ratios_floris[i]['baseline'] for i
-                            in range(len(self.energy_ratios_floris))]
-
-        wd_bias, success = opt.find_wd_bias_by_energy_ratios(
-            er_wd_list=wd_arrays,
-            er_scada_list=er_result_scada,
-            er_floris_list=er_result_floris,
-            search_range=self.opt_search_range,
-            search_dx=self.opt_search_dx
-            )
-
-        self.opt_wd_bias = wd_bias
-        self.opt_success = success
-
-        return wd_bias, success
-
     def _update_df_subset(self):
         printnow('Updated dataframes according to sliding window lock.')
         self.df_subset = self.df.loc[self._sw_lower_idx:self._sw_upper_idx].copy()
-        self.df_fi_subset = self.df_fi.loc[self._sw_lower_idx:self._sw_upper_idx].copy()
+        # self.df_fi_subset = self.df_fi.loc[self._sw_lower_idx:self._sw_upper_idx].copy()
 
     def _reset_results(self):
         # Set-up outputs
@@ -206,41 +179,42 @@ class bias_estimation():
             for i in range(df_upstream.shape[0])]
         self.upstream_turbs_ids = list(df_upstream.turbines)
 
-        # # Now mapping upstream turbine ids to test turbines
-        # x_turbs = fi.layout_x
-        # y_turbs = fi.layout_y
-        # test_turbines = self.test_turbines_subset
-        # max_radius = self.ref_turbine_maxrange
-        # upstream_turbs_ids_per_test_turb = [[] for _ in range(len(test_turbines))]
-        # for ii in range(len(test_turbines)):
-        #     ti = test_turbines[ii]
-        #     printnow('    Mapping upstream turbines per wind direction to each individual test turbine, ti = %d' % (ti))
-        #     turbs_in_radius = ftools.get_turbs_in_radius(x_turbs, y_turbs,
-        #                                                  ti, max_radius,
-        #                                                  include_itself=include_itself)
-
-        #     for wd_bin_ii in range(len(self.upstream_turbs_wds)):
-        #         upstream_turbs_local = [tii for tii in self.upstream_turbs_ids[wd_bin_ii] if (tii in turbs_in_radius)]
-        #         upstream_turbs_ids_per_test_turb[ii].append(upstream_turbs_local)
-
-        # # Save to self
-        # self.upstream_turbs_ids_per_test_turb = upstream_turbs_ids_per_test_turb
-
-    def _get_energy_ratios_allbins(self, N_btstrp=1):
+    def _get_energy_ratios_allbins(self, wd_bias=0.0, N_btstrp=1):
         wd_step = self.eo_wd_step
         ws_step = self.eo_ws_step
 
+        # Create dataframes: time-shifted df
+        df_subset_cor = self.df_subset.copy()
+        df_subset_cor['wd'] = wrap_360(df_subset_cor['wd'] - wd_bias)
+
+        # Set columns 'ws' and 'pow_ref' for df_subset_cor
+        df_subset_cor = self.df_ws_mapping_func(df_subset_cor)
+        df_subset_cor = self.df_pow_ref_mapping_func(df_subset_cor)
+
+        # Limit dataframe to ranges and remove NaN entries
+        df_subset_cor = dfm.filter_df_by_ws(df_subset_cor, self.df_ws_range)
+        df_subset_cor = df_subset_cor.dropna(subset=['time', 'wd',
+                                                     'ws', 'pow_ref'])
+        df_subset_cor  = df_subset_cor.reset_index(drop=True)
+
+        # Get FLORIS predictions
+        df_fi_subset = df_subset_cor[['time', 'wd', 'ws']].copy()
+        df_fi_subset, _ = ftools.calc_floris_approx(df=df_subset_cor, fi=self.fi,
+                                                    df_approx=self.df_fi_approx)
+        df_fi_subset = self.df_pow_ref_mapping_func(df_fi_subset)
+
+        # Initialize SCADA analysis class and add dataframes
         fsc = sca.scada_analysis(verbose=self.verbose)
-        fsc.add_df(self.df_subset, 'Measurement data')
-        fsc.add_df(self.df_fi_subset, 'FLORIS predictions')
+        fsc.add_df(df_subset_cor, 'Measurement data')
+        fsc.add_df(df_fi_subset, 'FLORIS predictions')
 
         test_turbines = self.test_turbines_subset
         self.energy_ratios_scada = [[] for _ in test_turbines]
         self.energy_ratios_floris = [[] for _ in test_turbines]
 
-        for ii in range(len(test_turbines)):
-            ti = test_turbines[ii]
-            printnow('  Determining energy ratio for test turbine = %03d' % (ti))
+        for ii, ti in enumerate(test_turbines):
+            printnow('  Determining energy ratio for wd_bias = ' +
+                     '%.3f and test turbine = %03d...' % (wd_bias, ti))
             df_energyratio_scada_ti = pd.DataFrame()
             df_energyratio_floris_ti = pd.DataFrame()
             for wd_bin_i in range(len(self.upstream_turbs_wds)):
@@ -265,6 +239,61 @@ class bias_estimation():
 
             self.energy_ratios_scada[ii] = df_energyratio_scada_ti
             self.energy_ratios_floris[ii] = df_energyratio_floris_ti
+
+        # Debugging: show all possible options
+        fp = ('/home/bdoekeme/python_scripts/wfc_owez/' +
+              'analysis/01_postprocessing/data/' +
+              '07_wdbias_filtered_data/'
+              'bias%+.3f' % (wd_bias) +
+              '_energyratio'
+            )
+        self.plot_energy_ratios(save_path=fp, format='png')
+        plt.close('all')
+
+        return None
+
+    def estimate_wd_bias(self, opt_search_range=(-180., 180.),
+                         opt_search_dx=1.0, opt_finish=opt.fmin,
+                         energy_ratio_N_btstrp=1):
+
+        printnow('Estimating the wind direction bias')
+        def cost_fun(wd_bias):
+            self._get_energy_ratios_allbins(wd_bias=wd_bias, N_btstrp=1)
+
+            # Unpack variables
+            # wd_arrays = [self.energy_ratios_scada[i]['wd_bin'] for i
+            #             in range(len(self.energy_ratios_scada))]
+            cost_array = np.full(len(self.energy_ratios_scada), np.nan)
+            for ii in range(len(self.energy_ratios_scada)):
+                y_scada = np.array(self.energy_ratios_scada[ii]['baseline'])
+                y_floris = np.array(self.energy_ratios_floris[ii]['baseline'])
+                ids = ~np.isnan(y_scada) & ~np.isnan(y_floris)
+                r, p = spst.pearsonr(y_scada[ids], y_floris[ids])
+                cost_array[ii] = -1. * r
+
+            cost = np.mean(cost_array)
+            return cost
+
+        dran = opt_search_range[1]-opt_search_range[0]
+        x_opt, J_opt, x, J = opt.brute(
+            func=cost_fun,
+            ranges=[opt_search_range],
+            Ns=int(np.ceil(dran/opt_search_dx) + 1),
+            full_output=True,
+            disp='iter',
+            finish=opt_finish)
+
+        wd_bias = x_opt
+        self.opt_wd_bias = wd_bias
+        self.opt_cost = J_opt
+        self.opt_wd_grid = x
+        self.opt_wd_cost = J
+
+        # End with optimal results and bootstrapping
+        self._get_energy_ratios_allbins(
+            wd_bias=x_opt, N_btstrp=energy_ratio_N_btstrp)
+
+        return x_opt, J_opt
 
     def plot_energy_ratios(self, save_path=None, format='png'):
         import matplotlib.pyplot as plt
