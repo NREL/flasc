1c541770a2e2e91611aceab0f85ea1604872ae82
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-05-12 13:43:35 -0600

    Fundamentally improve circular_statistics function, now accepting multidimensional arrays. Also, deals with wrapping in a much smarter way and therefore is computationally significantly more efficient

--- floris_scada_analysis/circular_statistics.py
+++ floris_scada_analysis/circular_statistics.py
@@ -12,54 +12,41 @@
 
 
 import numpy as np
-import pandas as pd
 
-from floris_scada_analysis.floris_tools import get_turbs_in_radius
-
-
-def wrap_360_deg(angles_in):
-    if isinstance(angles_in, float) or isinstance(angles_in, int):
-        angles_in = [angles_in]
-
-    angles_in = np.array([float(i) for i in angles_in])
-    while any(angles_in >= 360.):
-        angles_in[angles_in >= 360.] += - 360.
-    while any(angles_in < 0):
-        angles_in[angles_in < 0.] += 360.
-    return angles_in
+from floris.utilities import wrap_360
 
 
-def calc_wd_mean_radial(angles_array_deg):
+def calc_wd_mean_radial(angles_array_deg, axis=0):
     # Use unit vectors to calculate the mean
     wd_x = np.cos(angles_array_deg * np.pi / 180.)
     wd_y = np.sin(angles_array_deg * np.pi / 180.)
 
-    mean_wd = np.arctan2(np.sum(wd_y), np.sum(wd_x))
-    mean_wd = wrap_360_deg(mean_wd * 180. / np.pi)
+    mean_wd = np.arctan2(np.sum(wd_y, axis=axis),
+                         np.sum(wd_x, axis=axis))
+    mean_wd = wrap_360(mean_wd * 180. / np.pi)
 
     return mean_wd
 
 
-def calc_wd_mean_radial_list(angles_array_list):
-    if isinstance(angles_array_list, (pd.DataFrame, pd.Series)):
-        array = np.array(angles_array_list)
-    elif isinstance(angles_array_list, list):
-        array = np.vstack(angles_array_list).T
-    else:
-        array = np.array(angles_array_list)
+# def calc_wd_mean_radial_list(angles_array_list):
+#     if isinstance(angles_array_list, (pd.DataFrame, pd.Series)):
+#         array = np.array(angles_array_list)
+#     elif isinstance(angles_array_list, list):
+#         array = np.vstack(angles_array_list).T
+#     else:
+#         array = np.array(angles_array_list)
 
-    # Use unit vectors to calculate the mean
-    dir_x = np.cos(array * np.pi / 180.).sum(axis=1)
-    dir_y = np.sin(array * np.pi / 180.).sum(axis=1)
+#     # Use unit vectors to calculate the mean
+#     dir_x = np.cos(array * np.pi / 180.).sum(axis=1)
+#     dir_y = np.sin(array * np.pi / 180.).sum(axis=1)
 
-    mean_dirs = np.arctan2(dir_y, dir_x)
-    mean_out = wrap_360_deg(mean_dirs * 180. / np.pi)
+#     mean_dirs = np.arctan2(dir_y, dir_x)
+#     mean_out = wrap_360(mean_dirs * 180. / np.pi)
 
-    return mean_out
+#     return mean_out
 
 
-
-def calculate_wd_statistics(angles_array_deg):
+def calculate_wd_statistics(angles_array_deg, axis=0):
     """Determine statistical properties of an array of wind directions.
     This includes the mean of the array, the median, the standard deviation,
     the minimum value and the maximum value.
@@ -76,8 +63,7 @@ def calculate_wd_statistics(angles_array_deg):
     """
 
     # Preprocessing
-    angles_array_deg = wrap_360_deg(angles_array_deg)
-    angles_array_deg = angles_array_deg[~np.isnan(angles_array_deg)]
+    angles_array_deg = wrap_360(angles_array_deg)
 
     # Check for unique cases
     if angles_array_deg.shape[0] <= 0:
@@ -92,50 +78,23 @@ def calculate_wd_statistics(angles_array_deg):
         return mean_wd, median_wd, std_wd, min_wd, max_wd
 
     # Calculate the mean
-    mean_wd = calc_wd_mean_radial(angles_array_deg)
+    mean_wd = calc_wd_mean_radial(angles_array_deg, axis=axis)
 
-    # Calculate the standard deviation
-    var_wd_lin = np.abs(angles_array_deg-mean_wd)
-    var_wd_wrapped = np.abs(360-angles_array_deg-mean_wd)
-    var_wd = np.min([var_wd_lin, var_wd_wrapped], axis=0)
-    std_wd = np.sqrt((1./len(angles_array_deg)) * np.sum(var_wd**2.0))
+    # Upsample mean_wd for next calculations
+    new_shape = list(mean_wd.shape)
+    new_shape.insert(axis, 1)  # Add dimension at axis
+    new_shape = tuple(new_shape)
+    mean_wd_full = mean_wd.reshape(new_shape).repeat(
+        angles_array_deg.shape[axis], axis=axis)
 
-    # Calculate the min and max values
-    if mean_wd == 180.:
-        min_wd = np.min(angles_array_deg)
-        max_wd = np.max(angles_array_deg)
-    elif mean_wd < 180.:
-        min_range = np.append(angles_array_deg[angles_array_deg < mean_wd], -360. + angles_array_deg[angles_array_deg > (mean_wd+180.)])
-        min_wd = np.min(min_range)
-        max_range = angles_array_deg[angles_array_deg < (mean_wd+180.)]
-        max_wd = np.max(max_range)
-    elif mean_wd > 180.:
-        min_range = angles_array_deg[angles_array_deg > (mean_wd - 180.)]
-        min_wd = np.min(min_range)
-        max_range = np.append(angles_array_deg[angles_array_deg > mean_wd], 360. + angles_array_deg[angles_array_deg < mean_wd - 180.])
-        max_wd = np.max(max_range)
+    # Copy angles_array_deg and wrap values around its mean value
+    angles_wrp = angles_array_deg
+    angles_wrp[angles_wrp > (mean_wd_full + 180.)] += -360.
+    angles_wrp[angles_wrp < (mean_wd_full - 180.)] += 360.
 
-    # Wrap to [0, 360] deg
-    min_wd = wrap_360_deg(min_wd)
-    max_wd = wrap_360_deg(max_wd)
-
-    # Calculate median
-    if min_wd <= mean_wd and max_wd >= mean_wd:
-        median_wd = np.median(angles_array_deg)
-    elif min_wd > mean_wd and max_wd > mean_wd:
-        ids_wrapped = (angles_array_deg >= (min_wd - 1e-10))
-        angles_array_sorted = angles_array_deg[ids_wrapped] - 360.
-        angles_array_sorted = np.append(angles_array_sorted, angles_array_deg[~ids_wrapped])
-        median_wd = np.median(angles_array_sorted)
-    elif min_wd < mean_wd and max_wd < mean_wd:
-        ids_wrapped = (angles_array_deg <= (max_wd + 1e-10))
-        angles_array_sorted = angles_array_deg[ids_wrapped] + 360.
-        angles_array_sorted = np.append(angles_array_sorted, angles_array_deg[~ids_wrapped])
-        median_wd = np.median(angles_array_sorted)
-    else:
-        raise ValueError('Just a safety check. I think we accounted for all possible situations.')
-
-    # Wrap median to [0, 360]
-    median_wd = wrap_360_deg(median_wd)
+    median_wd = wrap_360(np.nanmedian(angles_wrp, axis=axis))
+    std_wd = np.nanstd(angles_wrp, axis=axis)
+    min_wd = wrap_360(np.nanmin(angles_wrp, axis=axis))
+    max_wd = wrap_360(np.nanmax(angles_wrp, axis=axis))
 
     return mean_wd, median_wd, std_wd, min_wd, max_wd
