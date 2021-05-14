dd08f9c0620907b4eabf5f7036acdc4119d9db2b
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-05-14 09:20:35 -0600

    Add logging.py function with a printnow() statement than can simply be used to replace print(). printnow() adds a timestamp before the string.

--- floris_scada_analysis/__init__.py
+++ floris_scada_analysis/__init__.py
@@ -6,9 +6,15 @@ __author__ = """Bart Doekemeijer"""
 __email__ = 'bart.doekemeijer@nrel.gov'
 __version__ = '0.1.0'
 
-from . import (circular_statistics, dataframe_filtering,
-               dataframe_manipulations, find_sensor_faults,
-               floris_sensitivity_analysis, floris_tools,
-               raw_data_importing, sqldatabase_management,
-               time_operations, turbulence_estimator,
+from . import (circular_statistics,
+               dataframe_filtering,
+               dataframe_manipulations,
+               find_sensor_faults,
+               floris_sensitivity_analysis,
+               floris_tools,
+               logging,
+               raw_data_importing,
+               sqldatabase_management,
+               time_operations,
+               turbulence_estimator,
                yaw_pow_fitting)
