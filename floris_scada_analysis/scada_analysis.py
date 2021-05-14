7a3eb30597c316fb297a38e499530afa353d8b91
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-05-14 09:22:21 -0600

    Add different masking options to scada_analysis. One can now easily change the focus on a dataframe to a different wind speed range and/or WD range and/or TI range and/or time range.

--- floris_scada_analysis/scada_analysis.py
+++ floris_scada_analysis/scada_analysis.py
@@ -18,25 +18,27 @@ from pandas.core.base import DataError
 
 from floris_scada_analysis import dataframe_manipulations as dfm
 from floris_scada_analysis import energy_ratio as er
+from floris_scada_analysis import time_operations as fsato
 
 
 class scada_analysis():
     """
     """
 
-    def __init__(self,
-                 ws_range_filter=None,
-                 wd_range_filter=None,
-                 ti_range_filter=None,
-                 verbose=False):
+    def __init__(self, verbose=False):
         self.df_list = []
         self.num_turbines = []
         self.turbine_names = []
 
-        self.ws_range = ws_range_filter
-        self.wd_range = wd_range_filter
-        self.ti_range = ti_range_filter
-
+        # Placeholders for masks
+        self.wd_range = None
+        self.wd_range_ids = []
+        self.ws_range = None
+        self.ws_range_ids = []
+        self.ti_range = None
+        self.ti_range_ids = []
+        self.time_range = None
+        self.time_range_ids = []
         self.verbose = verbose
 
         if verbose:
@@ -54,13 +56,13 @@ class scada_analysis():
 
         if self.num_turbines != num_turbines:
             print('Added dataframe seems to have a different number of ' +
-                  'turbines than the existing dataframe(s). Skipping ' + 
+                  'turbines than the existing dataframe(s). Skipping ' +
                   'addition.')
             return None
 
         if 'category' in df:
             if len(np.unique(df['category'])) > 2:
-                raise KeyError('More than 2 different category values ' + 
+                raise KeyError('More than 2 different category values ' +
                                'are found. Please limit yourself to 2.')
         else:
             print("No 'category' column found in df. Adding column with " +
@@ -71,13 +73,42 @@ class scada_analysis():
         categories = np.unique(df['category'])
         new_entry = dict({'df': df, 'name': name, 'categories': categories})
         self.df_list.append(new_entry)
-        self._apply_df_mask(-1)  # Last entry
+
+        default_ids = np.array([True for _ in range(df.shape[0])])
+        self.wd_range_ids.append(default_ids)
+        self.ws_range_ids.append(default_ids)
+        self.ti_range_ids.append(default_ids)
+        self.time_range_ids.append(default_ids)
+
+        # Force update mask for new dataframe
+        idx = len(self.df_list) - 1
+        wd_range_tmp = self.wd_range
+        ws_range_tmp = self.ws_range
+        ti_range_tmp = self.ti_range
+        time_range_tmp = self.time_range
+        self.wd_range = None
+        self.ws_range = None
+        self.ti_range = None
+        self.time_range = None
+        self.set_masks(ws_range=ws_range_tmp,
+                       wd_range=wd_range_tmp,
+                       ti_range=ti_range_tmp,
+                       time_range=time_range_tmp,
+                       df_ids=[idx])
 
     def remove_df(self, index):
         if self.verbose:
             print("Removing dataframe with name '" +
                   self.df_list[index]['name'] + "'.")
+
+        # Remove dataframe
         self.df_list.pop(index)
+
+        # Remove mask indices for this dataframe
+        self.wd_range_ids.pop(index)
+        self.ws_range_ids.pop(index)
+        self.ti_range_ids.pop(index)
+        self.time_range_ids.pop(index)
 
         if len(self.df_list) < 1:
             # Reset variables
@@ -90,46 +121,72 @@ class scada_analysis():
             for c in keys:
                 var = self.df_list[ii][c]
                 if isinstance(var, pd.DataFrame):
-                    print('  [' + str(ii) + '] '+ c + ': ' +
-                          'pd.Dataframe() with shape ', var.shape)
+                    print(
+                        '  [' + str(ii) + '] ' + c + ': ' +
+                        'pd.Dataframe() with shape ', var.shape
+                        )
                 else:
-                    print('  [' + str(ii) + '] '+ c + ': ', var)
+                    print('  [' + str(ii) + '] ' + c + ': ', var)
             print(' ')
 
-    def _apply_df_mask(self, ii):
-        df = self.df_list[ii]['df']#.copy()
-        if self.ws_range is not None:
-            df = dfm.filter_df_by_ws(df, self.ws_range)
-        if self.wd_range is not None:
-            df = dfm.filter_df_by_wd(df, self.wd_range)
-        if self.ti_range is not None:
-            if 'ti' in df.columns:
-                df = dfm.filter_df_by_ti(df, self.ti_range)
-        self.df_list[ii]['df_subset'] = df
-
-    def set_masks(self, ws_range=None, wd_range=None, ti_range=None):
+    def set_masks(self, ws_range=None, wd_range=None, ti_range=None,
+                  time_range=None, df_ids=None):
         if self.verbose:
-            print("Applying a mask over the dataframes and adding 'df_subset")
-        if ws_range is not None:
-            self.ws_range = ws_range
-        if wd_range is not None:
-            self.wd_range = wd_range
-        if ti_range is not None:
-            self.ti_range = ti_range
+            print("Extracting a mask over the df: 'df_subset'.")
 
-        for ii in range(len(self.df_list)):
-            self._apply_df_mask(ii)
-            if self.verbose:
-                print('df_subset[%d] is %d rows (df is %d rows).'
-                      % (ii, self.df_list[ii]['df_subset'].shape[0],
-                         self.df_list[ii]['df'].shape[0]))
+        if df_ids is None:
+            df_ids = range(len(self.df_list))
+        elif isinstance(df_ids, (int, np.integer, float)):
+            df_ids = [int(df_ids)]
+
+        if (ws_range is not None) and not (ws_range == self.ws_range):
+            self.ws_range = ws_range
+            for ii in df_ids:
+                df = self.df_list[ii]['df']
+                ids = (df['ws'] > ws_range[0]) & (df['ws'] <= ws_range[1])
+                self.ws_range_ids[ii] = np.array(ids)
+
+        if (wd_range is not None) and not (wd_range == self.wd_range):
+            self.wd_range = wd_range
+            for ii in df_ids:
+                df = self.df_list[ii]['df']
+                ids = (df['wd'] > wd_range[0]) & (df['wd'] <= wd_range[1])
+                self.wd_range_ids[ii] = np.array(ids)
+
+        if (ti_range is not None) and not (ti_range == self.ti_range):
+            self.ti_range = ti_range
+            for ii in df_ids:
+                df = self.df_list[ii]['df']
+                ids = (df['ti'] > ti_range[0]) & (df['ti'] <= ti_range[1])
+                self.ti_range_ids[ii] = np.array(ids)
+
+        if (time_range is not None) and not (time_range == self.time_range):
+            self.time_range = time_range
+            for ii in df_ids:
+                df = self.df_list[ii]['df']
+                ids = np.array([False for _ in range(df.shape[0])])
+                indices_out = fsato.find_window_in_time_array(
+                    df['time'], seek_time_windows=[list(time_range)])
+                ids[indices_out[0]] = True
+                self.time_range_ids[ii] = ids
+
+        # Update masked dataframe(s)
+        for ii in df_ids:
+            mask = (
+                (self.wd_range_ids[ii]) &
+                (self.ws_range_ids[ii]) &
+                (self.ti_range_ids[ii]) &
+                (self.time_range_ids[ii])
+            )
+            df_full = self.df_list[ii]['df']
+            self.df_list[ii]['df_subset'] = df_full[mask]
 
     def set_turbine_names(self, turbine_names):
         if not len(turbine_names) == self.num_turbines:
             raise DataError(
                 'The length of turbine_names is incorrect.'
-                'Length should  be %d (specified: %d).' % 
-                (self.num_turbines, len(turbine_names)))
+                'Length should  be %d (specified: %d).'
+                % (self.num_turbines, len(turbine_names)))
         self.turbine_names = turbine_names
 
     def clear_energy_ratio_results(self, ii):
@@ -177,7 +234,7 @@ class scada_analysis():
             test_turbine_names = [self.turbine_names[i] for i in
                                   self.df_list[ii]['er_test_turbines']]
 
-            ax.plot(result.wd_bin, result.baseline, 
+            ax.plot(result.wd_bin, result.baseline,
                     label=data_name+': '+categories[0])
             ax.fill_between(result.wd_bin, result.baseline_l,
                             result.baseline_u, alpha=0.15)
