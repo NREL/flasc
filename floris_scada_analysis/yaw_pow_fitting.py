0b09c3cda6ff9a3223c60e3053e325c63b158b02
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-05-24 10:12:27 -0600

    Significantly reduce and simplify yaw power curve fitting code to only work for a single turbine at a time. Also, move out a lot of internal code to outside to give the user more freedom on how to cut down a dataframe and how to set up a reference power production. Also, curve fitting now also includes y-shift/scaling freedom to deal with outliers that are much higher than 1 and where it becomes unclear to what variable the data should be normalized to have a value of 1.0 at optimal power production.

--- floris_scada_analysis/yaw_pow_fitting.py
+++ floris_scada_analysis/yaw_pow_fitting.py
@@ -23,67 +23,40 @@ from floris_scada_analysis import utilities as fsut
 
 
 class yaw_pow_fitting():
-    def __init__(self, df, df_upstream, ws_range=(6., 10.), turbine_list='all'):
+    def __init__(self, df, df_upstream=None, ti=0):#, turbine_list='all'):
         print('Initializing yaw power curve filtering object.')
         # Assign dataframes to self
-        self.df_upstream = df_upstream
-        self.set_df(df, ws_range)
+        # self.df_upstream = df_upstream
+        self.set_df(df, df_upstream, ti)
 
-        # Set turbines specified by user
-        self.set_turbine_mode(turbine_list)
+        # # Set turbines specified by user
+        # self.set_turbine_mode(turbine_list)
 
-    def set_df(self, df, ws_range=(6., 10.)):
-        if 'vane_000' not in df.columns:
-            raise KeyError('vane_000 not found in dataset.')
+    def set_df(self, df, df_upstream, ti):
+        # if 'vane_000' not in df.columns:
+        #     raise KeyError('vane_000 not found in dataset.')
 
-        # Get true total number of turbines
-        self.num_turbines_all = fsut.get_num_turbines(df)
-        self.full_turbine_list = range(self.num_turbines_all)
-
-        # Cut down dataframe by WS
-        df = dfm.set_ws_by_upstream_turbines(df, self.df_upstream)
-        df = df[df['ws'] > ws_range[0]]
-        df = df[df['ws'] < ws_range[1]]
-        df = df.reset_index(drop=True)
+        # # Get true total number of turbines
+        # self.num_turbines_all = fsut.get_num_turbines(df)
+        # self.full_turbine_list = range(self.num_turbines_all)
 
         # Set df using only the relevant columns
-        rlvnt_cols = ['wd']
-        rlvnt_cols.extend(['pow_%03d' % ti for ti in range(self.num_turbines_all)])
-        rlvnt_cols.extend(['vane_%03d' % ti for ti in range(self.num_turbines_all)])
-        self.df = df[rlvnt_cols]
+        rlvnt_cols = ['pow', 'pow_ref', 'vane', 'wd']
+        # rlvnt_cols.extend(['pow_%03d' % ti for ti in range(self.num_turbines_all)])
+        # rlvnt_cols.extend(['vane_%03d' % ti for ti in range(self.num_turbines_all)])
+        # rlvnt_cols = [c for c in rlvnt_cols if c in df.columns]
+        df = df[rlvnt_cols]
 
         # Reset output variables
-        self.bins_x_list = [[] for _ in range(self.num_turbines_all)]
-        self.bins_y_list = [[] for _ in range(self.num_turbines_all)]
-        self.bins_N_list = [[] for _ in range(self.num_turbines_all)]
+        self.bins_x = []
+        self.bins_y = []
+        self.bins_N = []
 
-        self.x_opt_list = [(None, None) for _ in range(self.num_turbines_all)]
-        self.bins_y_opt_list = [[] for _ in range(self.num_turbines_all)]
+        self.x_opt = [(None, None)]
+        self.bins_y_opt = []
 
-    def set_turbine_mode(self, turbine_list):
-        if isinstance(turbine_list, str):
-            if turbine_list == 'all':
-                num_turbines = fsut.get_num_turbines(self.df)
-                turbine_list = range(num_turbines)
-            else:
-                raise KeyError('Invalid turbine_list specified.')
-
-        self.turbine_list = turbine_list
-        self.num_turbines = len(turbine_list)
-
-    def calculate_curves(self, vane_bounds=(-15., 15.), dv=1.0, Pmin=10.0):
-        df_upstream = self.df_upstream
-        turbine_list = self.turbine_list
-
-        for ti in turbine_list:
-            print('Determining yaw-power curve for turbine %03d...' % ti)
-
-            print('  Retrieving relevant dataframe subset...')
-            rel_cols = ['wd', 'vane_%03d' % ti]
-            rel_cols.extend(['pow_%03d' % ti for ti in self.full_turbine_list])
-            df = self.df[rel_cols].copy()
-
-            # Filter by upstream conditions
+        # Filter by upstream conditions
+        if df_upstream is not None:
             df_upstr_ti = df_upstream[[ti in tl for tl in df_upstream['turbines']]]
             df_upstr_ti = df_upstr_ti.reset_index(drop=True)
             in_range = [False for _ in range(df.shape[0])]
@@ -93,114 +66,146 @@ class yaw_pow_fitting():
                 in_range = in_range | ((df['wd'] >= wd_min) & (df['wd'] <= wd_max))
             df = df.loc[in_range]
 
-            # Get reference power signals
-            print('  Cutting down dataframe by minimum reference power')
-            df = dfm.set_pow_ref_by_upstream_turbines(
-                df, df_upstream, exclude_turbs=[ti])
-            df = df[df['pow_ref'] > Pmin]
+        self.df = df
 
-            # Define vane and (normalized) power measurements
-            vane = wrap_180(np.array(df['vane_%03d' % ti]))
+    # def set_turbine_mode(self, turbine_list):
+    #     if isinstance(turbine_list, str):
+    #         if turbine_list == 'all':
+    #             num_turbines = fsut.get_num_turbines(self.df)
+    #             turbine_list = range(num_turbines)
+    #         else:
+    #             raise KeyError('Invalid turbine_list specified.')
 
-            # Filter for viable conditions
-            ids_good = ((vane >= vane_bounds[0]) &
-                        (vane <= vane_bounds[1]) &
-                        (df['pow_%03d' % ti] > Pmin))
-            vane = vane[ids_good]
-            Pnorm = df.loc[ids_good, 'pow_%03d' % ti] / df.loc[ids_good, 'pow_ref']
-            print('  Number of useful datapoints: %d.' % len(vane))
+    #     self.turbine_list = turbine_list
+    #     self.num_turbines = len(turbine_list)
 
-            # Bin data
-            print('  Binning data...')
-            bins_x = np.arange(vane_bounds[0], vane_bounds[1], dv)
-            bins_y = np.zeros_like(bins_x)
-            bins_N = np.zeros_like(bins_x)
+    def calculate_curves(self, vane_bounds=(-15., 15.), dv=1.0, Pmin=10.0):
+        df = self.df
+        # df_upstream = self.df_upstream
+        # turbine_list = self.turbine_list
 
-            for ii, edge_x_l in enumerate(bins_x):
-                edge_x_r = edge_x_l + dv
-                yi = Pnorm[(vane >= edge_x_l) & (vane < edge_x_r)]
-                bins_N[ii] = yi.shape[0]
-                bins_y[ii] = np.nanmean(yi)
-            bins_y = np.array(bins_y) / np.nanmax(bins_y[bins_N/np.max(bins_N) > 0.10])  # Normalize to 1
+        print('Determining yaw-power curve...')
+        # print('  Retrieving relevant dataframe subset...')
+        # rel_cols = ['wd', 'vane_%03d' % ti]
+        # rel_cols.extend(['pow_%03d' % ti for ti in self.full_turbine_list])
+        # rel_cols = [c for c in rel_cols if c in self.df.columns]
+        # df = self.df[rel_cols].copy()
 
-            self.bins_x_list[ti] = bins_x
-            self.bins_y_list[ti] = bins_y
-            self.bins_N_list[ti] = bins_N
 
-    def estimate_cos_pp_fit(self, opt_bias_range=(-15., 15.),
-                            opt_pp_range=(1.0, 10.0), opt_Ns=100):
+        # # Get reference power signals
+        # print('  Cutting down dataframe by minimum reference power')
+        # df = dfm.set_pow_ref_by_turbines(df, [])
+        # # df = dfm.set_pow_ref_by_upstream_turbines(
+        # #     df, df_upstream, exclude_turbs=[ti])
+        df = df[df['pow_ref'] > Pmin]
 
-        for ti in self.turbine_list:
-            bins_x = self.bins_x_list[ti]
-            bins_y = self.bins_y_list[ti]
-            bins_N = self.bins_N_list[ti]
+        # Define vane and (normalized) power measurements
+        vane = wrap_180(np.array(df['vane']))
 
-            if len(bins_x) <= 0:
-                raise ValueError('Please calculate curves using ' +
-                                 '.calculate_curves() before ' +
-                                 'estimating a fit for turbine %03d.' % ti)
+        # Filter for viable conditions
+        ids_good = ((vane >= vane_bounds[0]) & (vane <= vane_bounds[1]) &
+                    (df['pow'] > Pmin) & (df['pow_ref'] > Pmin))
+        vane = vane[ids_good]
+        Pnorm = df.loc[ids_good, 'pow'] / df.loc[ids_good, 'pow_ref']
+        print('  Number of useful datapoints: %d.' % len(vane))
 
-            # Define an approximating function
-            def approx_func(x):
-                y = np.cos((bins_x-x[0]) * np.pi / 180.)**x[1]
-                return y
+        # Bin data
+        print('  Binning data...')
+        bins_x = np.arange(vane_bounds[0], vane_bounds[1], dv)
+        bins_y = np.zeros_like(bins_x)
+        bins_N = np.zeros_like(bins_x)
 
-            # Define a cost function
-            def cost(x):
-                # x[0] is the x offset, x[1] is the cos coefficient
-                y_fit = approx_func(x)
-                J_fit = np.multiply(bins_N, (y_fit - bins_y)**2.)
-                J_sum = np.nanmean(J_fit)
-                return J_sum
+        for ii, edge_x_l in enumerate(bins_x):
+            edge_x_r = edge_x_l + dv
+            yi = Pnorm[(vane >= edge_x_l) & (vane < edge_x_r)]
+            bins_N[ii] = yi.shape[0]
+            bins_y[ii] = np.nanmean(yi)
 
-            print('Fitting a cos(x-x0)^pp curve to the data of turbine %03d...' % ti)
-            x_opt, J_opt, x, J = opt.brute(
-                func=cost,
-                ranges=(opt_bias_range, opt_pp_range),
-                Ns=opt_Ns,
-                finish=opt.fmin,
-                full_output=True,
-                disp=True
-                )
-            print('Turbine% 03d, x_opt: ' % ti, x_opt)
-            y_opt = approx_func(x_opt)
+        # if np.any(bins_N > 0):
+        #     bins_y = np.array(bins_y) / np.nanmax(bins_y[bins_N/np.max(bins_N) > 0.10])  # Normalize to 1
 
-            self.x_opt_list[ti] = x_opt
-            self.bins_y_opt_list[ti] = y_opt
+        self.bins_x = bins_x
+        self.bins_y = bins_y
+        self.bins_N = bins_N
 
-    def plot(self, save_path=None, fig_format='png', fig_dpi=250):
-        for ti in self.turbine_list:
-            bins_x = self.bins_x_list[ti]
-            bins_y = self.bins_y_list[ti]
-            bins_N = self.bins_N_list[ti]
+    def estimate_cos_pp_fit(self,
+                            opt_yshift_range=None,
+                            opt_bias_range=(-15., 15.),
+                            opt_pp_range=(1.0, 10.0), opt_Ns=40):
 
-            x_opt = self.x_opt_list[ti]
-            y_opt = self.bins_y_opt_list[ti]
+        # for ti in self.turbine_list:
+        bins_x = self.bins_x
+        bins_y = self.bins_y
+        bins_N = self.bins_N
 
-            if len(bins_x) <= 0:
-                raise ValueError('Please calculate curves using ' +
-                                 '.calculate_curves() before ' +
-                                 'plotting for turbine %03d.' % ti)
+        if len(bins_x) <= 0:
+            raise ValueError('Please calculate curves using ' +
+                                '.calculate_curves() before ' +
+                                'estimating a fit.')
 
-            # Plot bins and averaged curve
-            fig, ax = plt.subplots(nrows=2, sharex=True)
-            ax[0].plot(bins_x, bins_y, 'o-', label='Data')
-            if len(y_opt) > 0:
-                ax[0].plot(bins_x, y_opt, '--',
-                        label=('Fit (x0=%.3f, x1=%.3f)'
-                               % (x_opt[0], x_opt[1])))
-            ax[0].set_xlabel('Vane measurement (deg)')
-            ax[0].set_ylabel('Relative power production (-)')
-            ax[0].set_title('Turbine %03d' % ti)
-            ax[0].grid('minor')
-            ax[0].legend()
+        # Define an approximating function
+        def approx_func(x):
+            y = x[0] * np.cos((bins_x-x[1]) * np.pi / 180.)**x[2]
+            return y
 
-            ax[1].bar(bins_x, bins_N)
-            ax[1].set_xlabel('Vane measurement (deg)')
-            ax[1].set_ylabel('Number of data points (-)')
+        # Define a cost function
+        def cost(x):
+            # x[0] is the x offset, x[1] is the cos coefficient
+            y_fit = approx_func(x)
+            J_fit = np.multiply(bins_N, (y_fit - bins_y)**2.)
+            J_sum = np.nanmean(J_fit)
+            return J_sum
 
-            fig.tight_layout()
-            if save_path is not None:
-                os.makedirs(save_path, exist_ok=True)
-                plt.savefig(save_path + '/yawpowcurve_%03d.%s'
-                            % (ti, fig_format), dpi=fig_dpi)
+        if opt_yshift_range is None:
+            opt_yshift_range = (np.nanmin(bins_y), np.nanmax(bins_y))
+
+        print('Fitting a cos(x-x0)^pp curve to the data...')
+        x_opt, J_opt, x, J = opt.brute(
+            func=cost,
+            ranges=(opt_yshift_range,
+                    opt_bias_range, opt_pp_range),
+            Ns=opt_Ns,
+            finish=opt.fmin,
+            full_output=True,
+            disp=True
+            )
+        print('x_opt: ', x_opt)
+        y_opt = approx_func(x_opt)
+
+        self.x_opt = x_opt
+        self.bins_y_opt = y_opt
+
+    def plot(self, save_path=None, fig_dpi=250):
+        # for ti in self.turbine_list:
+        bins_x = self.bins_x
+        bins_y = self.bins_y
+        bins_N = self.bins_N
+
+        x_opt = self.x_opt
+        y_opt = self.bins_y_opt
+
+        if len(bins_x) <= 0:
+            raise ValueError('Please calculate curves using ' +
+                                '.calculate_curves() before ' +
+                                'plotting.')
+
+        # Plot bins and averaged curve
+        fig, ax = plt.subplots(nrows=2, sharex=True)
+        ax[0].plot(bins_x, bins_y, 'o-', label='Data')
+        if len(y_opt) > 0:
+            ax[0].plot(bins_x, y_opt, '--',
+                    label=('Fit (x0=%.3f, x1=%.3f, x2=%.3f)'
+                           % (x_opt[0], x_opt[1], x_opt[2])))
+        ax[0].set_xlabel('Vane measurement (deg)')
+        ax[0].set_ylabel('Relative power production (-)')
+        ax[0].grid('minor')
+        ax[0].legend()
+
+        ax[1].bar(bins_x, bins_N)
+        ax[1].set_xlabel('Vane measurement (deg)')
+        ax[1].set_ylabel('Number of data points (-)')
+
+        fig.tight_layout()
+        if save_path is not None:
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            plt.savefig(save_path, dpi=fig_dpi)
