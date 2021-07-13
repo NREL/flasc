1cebe8a889f8cf44fd64e338d89040240e980478
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-07-13 16:25:42 -0600

    Separate visualization function from energy ratio to an external library. Also, remove 'category' option in energy ratio. This all reduces the code a lot and makes things a lot more transparent

--- floris_scada_analysis/energy_ratio.py
+++ floris_scada_analysis/energy_ratio.py
@@ -12,12 +12,12 @@
 
 
 from itertools import product
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 
 from floris_scada_analysis import dataframe_manipulations as dfm
+from floris_scada_analysis import energy_ratio_visualization as ervis
 from floris_scada_analysis import utilities as fsut
 
 
@@ -63,21 +63,7 @@ class energy_ratio:
             # INFO: You can add such a column using dfm.set_pow_ref_by_*.
 
         # Copy minimal columns from df_in to df
-        if 'category' in df_in.columns:
-            df = df_in[['wd', 'ws', 'pow_ref', 'category']].copy()
-        else:
-            if self.verbose:
-                print("Did not find 'category' column. Adding one with all values 'baseline'.")
-            df = df_in[['wd', 'ws', 'pow_ref']].copy()
-            df['category'] = 'baseline'
-
-        # Check categories
-        self.categories = list(np.unique(df['category']))
-        if len(self.categories) > 2:
-            raise KeyError('Cannot have more than 2 categories in your dataframe.')
-        else:
-            if self.verbose:
-                print('Your dataframe has categories', self.categories)
+        df = df_in[['wd', 'ws', 'pow_ref']].copy()
 
         self.df_full = df_in  # Save reference to full dataframe
         self.df = df  # Minimal dataframe
@@ -187,44 +173,24 @@ class energy_ratio:
         elif self.verbose:
             print('  df has already been binned. Loading existing bins.')
 
-        # Extract the interesting parts for the energy ratio frame
-        rlvnt_cols = ['ws_bin', 'wd_bin', 'pow_ref', 'pow_test', 'category']
-        df = self.df[rlvnt_cols]
-
         # Define the energy frame class
-        energy_frame = Energy_Frame(df=df,
-                                    df_freq=self.df_freq,
-                                    category=self.categories)
+        energy_frame = energy_ratio_for_wd_array(df=df, df_freq=self.df_freq)
 
         # Retrieve the right function
-        if len(self.categories) == 1:
-            result = energy_frame.get_1_cat_energy_ratio_array_with_range(N=N)
-        else:
-            result = energy_frame.get_2_cat_energy_ratio_array_with_range(N=N)
-
+        result = energy_frame.get_energy_ratios(N=N)
         self.energy_ratio_out = result
         self.energy_ratio_N = N
 
         return result
 
     def plot_energy_ratio(self):
-        fig, ax = plt.subplots()
-        result = self.energy_ratio_out
-        ax.plot(result.wd_bin, result.baseline)
-        ax.fill_between(result.wd_bin, result.baseline_l, result.baseline_u, alpha=0.15)
-        if len(self.categories) > 1:
-            ax.plot(result.wd_bin, result.controlled)
-            ax.fill_between(result.wd_bin, result.controlled_l, result.controlled_u, alpha=0.15)
-        plt.legend(self.categories)
-        plt.title(str(['Test turbines:', self.test_turbines]))
-        plt.xlabel('Wind direction (degrees)')
-        plt.ylabel('Energy ratio (-)')
-        plt.grid(True)
-        return fig, ax
+        # Simple linker function for easy usage
+        ax = ervis.plot_single_curve(self.energy_ratio_out)
+        return ax
 
 
 # Legacy class from Paul Fleming
-class Energy_Frame:
+class energy_ratio_for_wd_array:
     """
     Top level analysis class for storing the data used across the analysis
 
@@ -234,9 +200,9 @@ class Energy_Frame:
         TBD
     """
 
-    def __init__(self, df, df_freq, category):
+    def __init__(self, df, df_freq):
 
-        self.df = df
+        self.df = df[['ws_bin', 'wd_bin', 'pow_ref', 'pow_test']]
 
         # Renormalize frequecies
         df_freq["observed_freq"] = df_freq.observed_freq / df_freq.observed_freq.sum()
@@ -247,68 +213,38 @@ class Energy_Frame:
         self.wd_bin = np.array(sorted(df.wd_bin.unique()))
         self.ws_bin = np.array(sorted(df.ws_bin.unique()))
 
-        self.category = category
-
-    def get_column(self, wd):
-        df_sub = (
-            self.df[self.df.wd_bin == wd]
-            .reset_index(drop=True)
-            .drop(["wd_bin"], axis="columns")
-        )
-        df_freq = (
-            self.df_freq[self.df_freq.wd_bin == wd]
-            .reset_index(drop=True)
-            .drop(["wd_bin"], axis="columns")
-        )
-        return Energy_Column(df_sub, df_freq, self.ws_bin, self.category)
-
-    def get_1_cat_energy_ratio_array_with_range(self, use_observed_freq=True, N=1):
+    def get_energy_ratios(self, use_observed_freq=True, N=1):
         result = np.zeros([len(self.wd_bin), 3])
         for wd_idx, wd in enumerate(self.wd_bin):
-            ec = self.get_column(wd)
-            result[wd_idx, :] = ec.get_1cat_energy_ratio_with_range(
+            df_sub = (
+                self.df[self.df.wd_bin == wd]
+                # .reset_index(drop=True)
+                # .drop(["wd_bin"], axis="columns")
+            )
+            df_freq = (
+                self.df_freq[self.df_freq.wd_bin == wd]
+                # .reset_index(drop=True)
+                # .drop(["wd_bin"], axis="columns")
+            )
+            ec = energy_ratio_for_single_wd_bin(df_sub, df_freq, self.ws_bin)
+            result[wd_idx, :] = ec.get_energy_ratio(
                 use_observed_freq=use_observed_freq, N=N
             )
+
         df_res = pd.DataFrame(result, columns=["baseline", "baseline_l", "baseline_u"])
+
         df_res["wd_bin"] = self.wd_bin
         _, df_res["N_bin"] = np.unique(self.df.wd_bin, return_counts=True)
-        return df_res
 
-    def get_2_cat_energy_ratio_array_with_range(self, use_observed_freq=True, N=1):
-        result = np.zeros([len(self.wd_bin), 12])
-        for wd_idx, wd in enumerate(self.wd_bin):
-            ec = self.get_column(wd)
-            result[wd_idx, :] = ec.get_2cat_energy_ratio_with_range(
-                use_observed_freq=use_observed_freq, N=N
-            )
-
-        df_res = pd.DataFrame(
-            result,
-            columns=[
-                "baseline",
-                "baseline_l",
-                "baseline_u",
-                "controlled",
-                "controlled_l",
-                "controlled_u",
-                "diff",
-                "diff_l",
-                "diff_u",
-                "per",
-                "per_l",
-                "per_u",
-            ],
-        )
-        df_res["wd_bin"] = self.wd_bin
-        _, df_res["N_bin"] = np.unique(self.df.wd_bin, return_counts=True)
         return df_res
 
 
-# Legacy class from Paul
-class Energy_Column:
-    def __init__(self, df, df_freq, ws_bin, category):
+# Legacy class from Paul Fleming
+class energy_ratio_for_single_wd_bin:
+    def __init__(self, df, df_freq, ws_bin):
 
-        self.df = df
+        # Copy minimal dataframe
+        self.df = df[["ws_bin", "pow_ref", "pow_test"]]
 
         # Renormalize frequecies
         df_freq["observed_freq"] = df_freq.observed_freq / df_freq.observed_freq.sum()
@@ -317,41 +253,25 @@ class Energy_Column:
 
         # Save the bins
         self.ws_bin = ws_bin
-        self.category = category
 
-    def get_energy_ratio(self, use_observed_freq=True, randomize_df=False):
+    def _get_energy_ratio_nominal(self, use_observed_freq=True, randomize_df=False):
 
         # Local copies
         df = self.df #.copy()
         df_freq = self.df_freq #.copy()
-        ws_bin = self.ws_bin
-        category = self.category
 
         # If resampling for boot-strapping
         if randomize_df:
             df = df.sample(frac=1, replace=True)
 
-        # Remove bins with unmatched categories
-        for ws in ws_bin:
-            for cg_idx, cg in enumerate(category):
-                if not (((df.ws_bin == ws) & (df.category == cg)).any()):
-                    # print('Cat: %s is missing %d m/s, removing' % (cg, ws))
-                    df = df[df.ws_bin != ws]
-                    df_freq = df_freq[df_freq.ws_bin != ws]
-
-        # Check for empty frame
-        if df.shape[0] == 0:
-            return np.zeros(len(category)) * np.nan
-
         df_group = (
-            df[["ws_bin", "category", "pow_ref", "pow_test"]]
-            .groupby(["ws_bin", "category"])
+            df[["ws_bin", "pow_ref", "pow_test"]]
+            .groupby(["ws_bin"])
             .mean()
         )
-        df_ref = df_group[["pow_ref"]].unstack()
-        df_ref.columns = [c[1] for c in df_ref.columns]
-        df_test = df_group[["pow_test"]].unstack()
-        df_test.columns = [c[1] for c in df_test.columns]
+
+        df_ref = df_group["pow_ref"]
+        df_test = df_group["pow_test"]
 
         df_freq = (
             df_freq[["ws_bin", "observed_freq", "annual_freq"]]
@@ -360,120 +280,58 @@ class Energy_Column:
         )
 
         # Ensure that dimensions match
-        ws_bins_ref = np.array(df_ref.reset_index()['ws_bin'])
-        ws_bins_test = np.array(df_ref.reset_index()['ws_bin'])
-        ws_bins_freq = np.array(df_freq.reset_index()['ws_bin'])
-        if (not np.array_equal(ws_bins_ref, ws_bins_test) or
-            not np.array_equal(ws_bins_ref, ws_bins_freq)):
-            ws_bins_array = np.concatenate([ws_bins_ref, ws_bins_test, ws_bins_freq])
+        ws_bins = np.array(df_ref.index, dtype=float)
+        ws_bins_freq = np.array(df_freq.index, dtype=float)
+        if not np.array_equal(ws_bins, ws_bins_freq):
+            ws_bins_array = np.concatenate([ws_bins, ws_bins_freq])
             ws_bins_array = np.unique(ws_bins_array)
 
-            y = np.interp(ws_bins_array, df_ref.index, df_ref['baseline'])
-            df_ref = pd.DataFrame({'ws_bin': ws_bins_array, 'baseline': y})
-            df_ref = df_ref.set_index('ws_bin')
+            y = np.interp(ws_bins_array, ws_bins, df_ref)
+            df_ref = pd.Series(data=y, index=ws_bins_array, name="pow_ref")
 
-            y = np.interp(ws_bins_array, df_test.index, df_test['baseline'])
-            df_test = pd.DataFrame({'ws_bin': ws_bins_array, 'baseline': y})
-            df_test = df_test.set_index('ws_bin')
+            y = np.interp(ws_bins_array, ws_bins, df_test)
+            df_test = pd.Series(data=y, index=ws_bins_array, name="pow_test")
 
             # df_freq = df_freq.append({'ws_bin': i, 'observed_freq': np.nan, 'annual_freq': np.nan}, ignore_index=True)
             # df_freq = df_freq.sort(by='ws_bin')
 
-        results = np.zeros(len(category))
         if use_observed_freq:
             freq_signal = "observed_freq"
         else:
             freq_signal = "annual_freq"
-        for catg_idx, catg in enumerate(category):
-            ref_energy = compute_expectation(df_ref[catg], df_freq[freq_signal])
-            test_energy = compute_expectation(df_test[catg], df_freq[freq_signal])
-            results[catg_idx] = test_energy / ref_energy
 
-        return results
+        ref_energy = compute_expectation(df_ref, df_freq[freq_signal])
+        test_energy = compute_expectation(df_test, df_freq[freq_signal])
+        energy_ratio = test_energy / ref_energy
 
-    def get_1cat_energy_ratio(self, use_observed_freq=True, randomize_df=False):
+        return energy_ratio
 
-        results = self.get_energy_ratio(
-            use_observed_freq=use_observed_freq, randomize_df=randomize_df
-        )
-
-        # Return with the difference as a convience
-        return np.array([results[0]])
-
-    def get_1cat_energy_ratio_with_range(
-        self, use_observed_freq=True, N=100, percentiles=[10, 90]
-    ):
+    def get_energy_ratio(self, use_observed_freq=True, N=1, percentiles=[10, 90]):
 
         # Get central results
-        results = self.get_1cat_energy_ratio(
+        results = self._get_energy_ratio_nominal(
             use_observed_freq=use_observed_freq, randomize_df=False
         )
 
-        # Get a bootstrap sample of range
-        bootstrap_results = np.zeros([N, 1])
-        for i in range(N):
-            bootstrap_results[i, :] = self.get_1cat_energy_ratio(
-                use_observed_freq=use_observed_freq, randomize_df=True
+        # Add bootstrapping results, if necessary
+        if N <= 1:
+            results_array = np.array([results, results, results])
+        else:
+            # Get a bootstrap sample of range
+            bootstrap_results = np.zeros([N, 1])
+            for i in range(N):
+                bootstrap_results[i, :] = self._get_energy_ratio_nominal(
+                    use_observed_freq=use_observed_freq,
+                    randomize_df=True
+                )
+
+            # Return the results in the order used in previous versions
+            results_array = np.array(
+                [
+                    results,
+                    np.nanpercentile(bootstrap_results[:, 0], percentiles)[0],
+                    np.nanpercentile(bootstrap_results[:, 0], percentiles)[1],
+                ]
             )
-
-        # Return the results in the order used in previous versions
-        results_array = np.array(
-            [
-                results[0],
-                np.nanpercentile(bootstrap_results[:, 0], percentiles)[0],
-                np.nanpercentile(bootstrap_results[:, 0], percentiles)[1],
-            ]
-        )
-        return results_array
-
-    def get_2cat_energy_ratio(self, use_observed_freq=True, randomize_df=False):
-
-        results = self.get_energy_ratio(
-            use_observed_freq=use_observed_freq, randomize_df=randomize_df
-        )
-
-        # Return with the difference as a convience
-        return np.array(
-            [
-                results[0],
-                results[1],
-                results[1] - results[0],
-                100 * (results[1] - results[0]) / results[0],
-            ]
-        )
-
-    def get_2cat_energy_ratio_with_range(
-        self, use_observed_freq=True, N=100, percentiles=[10, 90]
-    ):
-
-        # Get central results
-        results = self.get_2cat_energy_ratio(
-            use_observed_freq=use_observed_freq, randomize_df=False
-        )
-
-        # Get a bootstrap sample of range
-        bootstrap_results = np.zeros([N, 4])
-        for i in range(N):
-            bootstrap_results[i, :] = self.get_2cat_energy_ratio(
-                use_observed_freq=use_observed_freq, randomize_df=True
-            )
-
-        # Return the results in the order used in previous versions
-        results_array = np.array(
-            [
-                results[0],
-                np.nanpercentile(bootstrap_results[:, 0], percentiles)[0],
-                np.nanpercentile(bootstrap_results[:, 0], percentiles)[1],
-                results[1],
-                np.nanpercentile(bootstrap_results[:, 1], percentiles)[0],
-                np.nanpercentile(bootstrap_results[:, 1], percentiles)[1],
-                results[2],
-                np.nanpercentile(bootstrap_results[:, 2], percentiles)[0],
-                np.nanpercentile(bootstrap_results[:, 2], percentiles)[1],
-                results[3],
-                np.nanpercentile(bootstrap_results[:, 3], percentiles)[0],
-                np.nanpercentile(bootstrap_results[:, 3], percentiles)[1],
-            ]
-        )
 
         return results_array
