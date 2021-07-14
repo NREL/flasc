0194ed3e45d35f16afc79a818970096ebd424c75
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2021-07-14 14:55:14 -0600

    Allow arrays of energy ratio results to be inserted into the visualization function.

--- floris_scada_analysis/energy_ratio/energy_ratio_visualization.py
+++ floris_scada_analysis/energy_ratio/energy_ratio_visualization.py
@@ -11,20 +11,63 @@
 # the License.
 
 
+import numpy as np
+
 import matplotlib.pyplot as plt
 
 
-def plot_single_curve(df_energy_ratios, ax=None):
-    df = df_energy_ratios
+def plot(energy_ratios, labels=None):
+    # Format inputs if single case is inserted vs. lists
+    if not isinstance(energy_ratios, (list, tuple)):
+        energy_ratios = [energy_ratios]
+        if isinstance(labels, str):
+            labels = [labels]
 
-    # Create new figure if axis handle is None
-    if ax is None:
-        fig, ax = plt.subplots()
+    if labels is None:
+        labels = [None for _ in energy_ratios]
 
-    ax.plot(df.wd_bin, df.baseline)
-    ax.fill_between(df.wd_bin, df.baseline_l, df.baseline_u, alpha=0.15)
-    plt.xlabel('Wind direction (degrees)')
-    plt.ylabel('Energy ratio (-)')
+    N = len(energy_ratios)
+    fig, ax = plt.subplots(nrows=N, sharex=True)
+
+    # Calculate bar width for bin counts
+    bar_width = (0.7 / N) * np.min(
+        [np.diff(er["wd_bin"])[0] for er in energy_ratios]
+    )
+
+    for ii, df in enumerate(energy_ratios):
+        # Get x-axis values
+        x = np.array(df["wd_bin"], dtype=float)
+
+        # Plot horizontal black line at 1.
+        xlims = [np.min(x) - 4., np.max(x) + 4.]
+        ax[0].plot(xlims, [1., 1.], color='black')
+
+        # Plot energy ratios
+        ax[0].plot(x, df["baseline"], '-o', label=labels[ii])
+
+        # Plot uncertainty bounds from bootstrapping
+        ax[0].fill_between(x, df["baseline_l"], df["baseline_u"], alpha=0.15)
+
+        # Plot the bin count
+        ax[1].bar(x-(ii-N/2)*bar_width, df["N_bin"], width=bar_width)
+
+    # Format the energy ratio plot
+    ax[0].set_ylabel('Energy ratio (-)')
+    ax[0].grid(b=True, which='major', axis='both', color='gray')
+    ax[0].grid(b=True, which='minor', axis='both', color='lightgray')
+    ax[0].minorticks_on()
     plt.grid(True)
 
-    return ax
+    if labels[0] is not None:
+        ax[0].legend()
+
+    # Format the bin count plot
+    ax[1].grid(b=True, which='major', axis='both', color='gray')
+    ax[1].grid(b=True, which='minor', axis='both', color='lightgray')
+    ax[1].set_xlabel('Wind direction (deg)')
+    ax[1].set_ylabel('Number of data points (-)')
+
+    # Enforce a tight layout
+    plt.tight_layout()
+
+    return fig, ax
