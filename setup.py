051628a29d967c936f5a90b518723f17e8128a13
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2022-02-07 11:13:35 -0700

    add floris as an explicit dependency. remove windows from tests.

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
+    'floris==2.4',
     'feather-format>=0.4.1',
     'matplotlib>=3',
     'numba',
