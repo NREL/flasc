c5d1c83b2011347f35bae3c1d7575412e7a3708e
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2022-03-02 13:20:56 -0700

    Remove Python 3.7 from automated test environments.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'floris>=3.0rc4',
+    'floris>=3.0',
     'feather-format>=0.4.1',
     'matplotlib>=3',
     'numpy==1.21',
