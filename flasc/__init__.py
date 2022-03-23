6e9735640e12b3ed0670b4c9dbd4c066ac7a5453
Bart Doekemeijer <bart.doekemeijer@nrel.gov>
2022-03-23 16:00:14 -0600

    Add wake steering module

--- flasc/__init__.py
+++ flasc/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Bart Doekemeijer"""
 __email__ = 'bart.doekemeijer@nrel.gov'
-__version__ = '0.1.0'
+__version__ = '1.0'
 
 from pathlib import Path
 
@@ -14,10 +14,12 @@ from . import (
     model_estimation,
     raw_data_handling,
     turbine_analysis,
+    wake_steering,
     circular_statistics,
     floris_tools,
     optimization,
     time_operations,
-    utilities
+    utilities,
+    visualization
 )
 
