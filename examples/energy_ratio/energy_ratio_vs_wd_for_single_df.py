83a1be9dee0f1e1601381101d3eed166f84746b9
Paul <paul.fleming@nrel.gov>
2023-02-03 16:30:36 -0700

    re-sort imports

--- examples/energy_ratio/energy_ratio_vs_wd_for_single_df.py
+++ examples/energy_ratio/energy_ratio_vs_wd_for_single_df.py
@@ -11,15 +11,15 @@
 # the License.
 
 
-import matplotlib.pyplot as plt
 import os
-import pandas as pd
 
+import matplotlib.pyplot as plt
+import pandas as pd
 from floris import tools as wfct
-from flasc.energy_ratio import energy_ratio
-from flasc.dataframe_operations import \
-    dataframe_manipulations as dfm
+
 from flasc import floris_tools as fsatools
+from flasc.dataframe_operations import dataframe_manipulations as dfm
+from flasc.energy_ratio import energy_ratio
 
 
 def load_data():
