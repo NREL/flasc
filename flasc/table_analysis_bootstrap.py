5a2d4e6e684f7c45257e4460d6da0a5220ffaa1d
Paul <paul.fleming@nrel.gov>
2023-02-17 16:41:41 -0700

    Update bootstrapping

--- flasc/table_analysis_bootstrap.py
+++ flasc/table_analysis_bootstrap.py
@@ -35,7 +35,7 @@ class TableAnalysisBootstrap():
         self.nominal_table = None
         self.bootstrap_tables = None
 
-    def add_df(self, case_df, case_name):
+    def add_df(self, case_df, case_name, seaborn_color_palette='colorblind'):
 
         df = case_df.copy()
 
@@ -43,6 +43,9 @@ class TableAnalysisBootstrap():
         self.case_df_list.append(df)
         self.case_names.append(case_name)
         self.n_cases += 1
+
+        # Update the color pallette for plotting
+        self.case_colors = sns.color_palette(seaborn_color_palette, self.n_cases)
 
 
     def build_bootstrap_tables(self,
@@ -97,7 +100,7 @@ class TableAnalysisBootstrap():
             table_analysis = TableAnalysis(ws_step=self.ws_step, wd_step=self.wd_step, minutes_per_point = self.minutes_per_point)
 
             for c_i in range(self.n_cases):
-                df_sampled = pd.concat([block_data_frame_list[c_i][b_i] for b_i in sampled_blocks])
+                df_sampled = pd.concat([block_data_frame_list[c_i][sb_i] for sb_i in sampled_blocks])
 
                 # Add the dataframe to the table analysis object
                 table_analysis.add_df(df_sampled, self.case_names[c_i])
@@ -106,12 +109,12 @@ class TableAnalysisBootstrap():
             self.bootstrap_tables[b_i] = table_analysis
 
 
-    def get_energy_in_range_per_turbine(self, 
-        turbine, 
-        ws_min=None, 
-        ws_max=None, 
-        wd_min=None,
-        wd_max=None,
+    def get_energy_in_range(self, 
+        turbine_list=None, 
+        ws_min=0., 
+        ws_max=100., 
+        wd_min=0.,
+        wd_max=360.,
         min_points_per_bin=1,
         mean_or_median='mean',
         frequency_matrix_type='turbine',
@@ -122,94 +125,259 @@ class TableAnalysisBootstrap():
             raise ValueError("Tables are not built. Run build_bootstrap_tables() first.")
 
         # Get the energy in range for the nominal table
-        energy_in_range_nominal = self.nominal_table.get_energy_in_range_per_turbine(turbine,
-                                                                                        ws_min=ws_min, 
-                ws_max=ws_max, 
-                wd_min=wd_min,
-                wd_max=wd_max,
-                min_points_per_bin=min_points_per_bin,
-                mean_or_median=mean_or_median,
-                frequency_matrix_type=frequency_matrix_type,)
+        energy_nominal = self.nominal_table.get_energy_in_range(
+            turbine_list=turbine_list,
+            ws_min=ws_min,
+            ws_max=ws_max,
+            wd_min=wd_min,
+            wd_max=wd_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+        )
 
         # Initialize the energy as a list of zeros whose dimensions are n_bootstraps x n_cases
-        energy_in_range_bootstrap = np.zeros([self.n_cases, self.n_bootstraps])
+        energy_bootstrap = np.zeros([self.n_cases, self.n_bootstraps])
 
         # Loop through the bootstraps and add the energy in range
         for b_i in range(self.n_bootstraps):
-            energy_in_range_bootstrap[:,b_i] = self.bootstrap_tables[b_i].get_energy_in_range_per_turbine(turbine, 
-                ws_min=ws_min, 
-                ws_max=ws_max, 
-                wd_min=wd_min,
-                wd_max=wd_max,
-                min_points_per_bin=min_points_per_bin,
-                mean_or_median=mean_or_median,
-                frequency_matrix_type=frequency_matrix_type,)
+            energy_bootstrap[:,b_i] = self.bootstrap_tables[b_i].get_energy_in_range(
+            turbine_list=turbine_list,
+            ws_min=ws_min,
+            ws_max=ws_max,
+            wd_min=wd_min,
+            wd_max=wd_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+        )
 
         # Built the results array
         results_array = np.array(
                 [
-                energy_in_range_nominal,
-                np.percentile(energy_in_range_bootstrap, percentiles[0], axis=1),
-                np.percentile(energy_in_range_bootstrap, percentiles[1], axis=1),
+                energy_nominal,
+                np.percentile(energy_bootstrap, percentiles[0], axis=1),
+                np.percentile(energy_bootstrap, percentiles[1], axis=1),
                 ]
-        ).T # Transpose the array so that the columns are nominal, lower, and upper 
-            # and 0-index is the case
+        )
+        
+        # Transpose to dimensions are order cases then [nominal, lower, upper]
+        results_array = np.transpose(results_array, (1, 0))
 
         return results_array
 
-    def get_energy_in_range_across_turbines(self,
-                                            turbine_list= None,
-                                            ws_min=None, 
-                                            ws_max=None, 
-                                            wd_min=None,
-                                            wd_max=None,
-                                            min_points_per_bin=1,
-                                            mean_or_median='mean',
-                                            frequency_matrix_type='turbine',
-                                            percentiles=[5.0, 95.0]):
-            
+    def get_energy_per_wd_bin(self, 
+        turbine_list=None, 
+        ws_min=0., 
+        ws_max=100., 
+        min_points_per_bin=1,
+        mean_or_median='mean',
+        frequency_matrix_type='turbine',
+        percentiles=[5.0, 95.0],):
+
         # Check that the tables is not None
         if self.bootstrap_tables is None or self.nominal_table is None:
             raise ValueError("Tables are not built. Run build_bootstrap_tables() first.")
         
-        # Get the energy in range for the nominal table
-        energy_in_range_nominal = self.nominal_table.get_energy_in_range_across_turbines(turbine_list=None,
-                                                                                        ws_min=ws_min, 
-                ws_max=ws_max, 
-                wd_min=wd_min,
-                wd_max=wd_max,
-                min_points_per_bin=min_points_per_bin,
-                mean_or_median=mean_or_median,
-                frequency_matrix_type=frequency_matrix_type,)
+        # Grab values for conveience
+        n_wd_bins = self.nominal_table.n_wd_bins
 
-        # Initialize the energy as a list of zeros whose dimensions are n_bootstraps x n_cases
-        energy_in_range_bootstrap = np.zeros([self.n_cases, self.n_bootstraps])
+        # Get the energy in range for the nominal table
+        energy_nominal = self.nominal_table.get_energy_per_wd_bin(
+            turbine_list=turbine_list,
+            ws_min=ws_min,
+            ws_max=ws_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+        )
+
+        # Initialize the energy as a list of zeros whose dimensions are n_bootstraps x n_cases x n_ws_bins
+        energy_bootstrap = np.zeros([self.n_cases, self.n_bootstraps, n_wd_bins])
 
         # Loop through the bootstraps and add the energy in range
         for b_i in range(self.n_bootstraps):
-            energy_in_range_bootstrap[:,b_i] = self.bootstrap_tables[b_i].get_energy_in_range_across_turbines(turbine_list, 
-                ws_min=ws_min, 
-                ws_max=ws_max, 
-                wd_min=wd_min,
-                wd_max=wd_max,
-                min_points_per_bin=min_points_per_bin,
-                mean_or_median=mean_or_median,
-                frequency_matrix_type=frequency_matrix_type,)
+            energy_bootstrap[:,b_i, :] = self.bootstrap_tables[b_i].get_energy_per_wd_bin(
+            turbine_list=turbine_list,
+            ws_min=ws_min,
+            ws_max=ws_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+        )
 
         # Built the results array
         results_array = np.array(
                 [
-                energy_in_range_nominal,
-                np.percentile(energy_in_range_bootstrap, percentiles[0], axis=1),
-                np.percentile(energy_in_range_bootstrap, percentiles[1], axis=1),
+                energy_nominal,
+                np.percentile(energy_bootstrap, percentiles[0], axis=1),
+                np.percentile(energy_bootstrap, percentiles[1], axis=1),
                 ]
-        ).T # Transpose the array so that the columns are nominal, lower, and upper 
-            # and 0-index is the case
+        ) 
+
+        # Transpose to dimensions are order cases, wd, [nominal, lower, upper]
+        results_array = np.transpose(results_array, (1, 2, 0))
 
         return results_array
 
 
+    def get_energy_per_ws_bin(self, 
+        turbine_list=None, 
+        wd_min=0.,
+        wd_max=360.,
+        min_points_per_bin=1,
+        mean_or_median='mean',
+        frequency_matrix_type='turbine',
+        percentiles=[5.0, 95.0],):
 
+        # Check that the tables is not None
+        if self.bootstrap_tables is None or self.nominal_table is None:
+            raise ValueError("Tables are not built. Run build_bootstrap_tables() first.")
+        
+        # Grab values for conveience
+        n_ws_bins = self.nominal_table.n_ws_bins
+
+        # Get the energy in range for the nominal table
+        energy_nominal = self.nominal_table.get_energy_per_ws_bin(
+            turbine_list=turbine_list,
+            wd_min=wd_min,
+            wd_max=wd_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+        )
+
+        # Initialize the energy as a list of zeros whose dimensions are n_bootstraps x n_cases x n_ws_bins
+        energy_bootstrap = np.zeros([self.n_cases, self.n_bootstraps, n_ws_bins])
+
+        # Loop through the bootstraps and add the energy in range
+        for b_i in range(self.n_bootstraps):
+            energy_bootstrap[:,b_i, :] = self.bootstrap_tables[b_i].get_energy_per_ws_bin(
+            turbine_list=turbine_list,
+            wd_min=wd_min,
+            wd_max=wd_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+        )
+
+        # Built the results array
+        results_array = np.array(
+                [
+                energy_nominal,
+                np.percentile(energy_bootstrap, percentiles[0], axis=1),
+                np.percentile(energy_bootstrap, percentiles[1], axis=1),
+                ]
+        ) 
+
+        # Transpose to dimensions are order cases, ws, [nominal, lower, upper]
+        results_array = np.transpose(results_array, (1, 2, 0))
+
+        return results_array
+    
+    def plot_energy_per_wd_bin(self,
+            turbine_list=None, 
+            ws_min=0., 
+            ws_max=100., 
+            min_points_per_bin=1,
+            mean_or_median='mean',
+            frequency_matrix_type='turbine',
+            percentiles=[5.0, 95.0],
+            ax=None, 
+            **kwargs,
+    ):
+        
+        # Get the results array
+        results_array = self.get_energy_per_wd_bin(
+            turbine_list=turbine_list,
+            ws_min=ws_min,
+            ws_max=ws_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+            percentiles=percentiles,
+        )
+
+        # Grab values for conveience
+        wd_bin_centers = self.nominal_table.wd_bin_centers
+
+        # If the axis is not provided, create a new one
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        # Loop through the cases and plot the nominal and the percentiles
+        for c_i in range(self.n_cases):
+            ax.plot(wd_bin_centers, 
+                    results_array[c_i, :, 0], 
+                    label=self.case_names[c_i],
+                    color=self.case_colors[c_i],)
+            ax.fill_between(wd_bin_centers, 
+                            results_array[c_i, :, 1],
+                              results_array[c_i, :, 2], 
+                              alpha=0.3,
+                              color=self.case_colors[c_i],)
+
+        # Set the labels
+        ax.set_xlabel('Wind Direction [deg]')
+        ax.set_ylabel('Energy [kWh]')
+        ax.set_title('Energy per Wind Direction Bin')
+        ax.grid(True)
+        ax.legend()
+
+        return ax
+        
+
+    def plot_energy_per_ws_bin(self,
+            turbine_list=None, 
+            wd_min=0.,
+            wd_max=360.,
+            min_points_per_bin=1,
+            mean_or_median='mean',
+            frequency_matrix_type='turbine',
+            percentiles=[5.0, 95.0],
+            ax=None, 
+            **kwargs,
+    ):
+        
+        # Get the results array
+        results_array = self.get_energy_per_ws_bin(
+            turbine_list=turbine_list,
+            wd_min=wd_min,
+            wd_max=wd_max,
+            min_points_per_bin=min_points_per_bin,
+            mean_or_median=mean_or_median,
+            frequency_matrix_type=frequency_matrix_type,
+            percentiles=percentiles,
+        )
+
+        # Grab values for conveience
+        ws_bin_centers = self.nominal_table.ws_bin_centers
+
+        # If the axis is not provided, create a new one
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        # Loop through the cases and plot the nominal and the percentiles
+        for c_i in range(self.n_cases):
+            ax.plot(ws_bin_centers, 
+                    results_array[c_i, :, 0], 
+                    label=self.case_names[c_i],
+                    color=self.case_colors[c_i],)
+            ax.fill_between(ws_bin_centers, 
+                            results_array[c_i, :, 1],
+                              results_array[c_i, :, 2], 
+                              alpha=0.3,
+                              color=self.case_colors[c_i],)
+
+        # Set the labels
+        ax.set_xlabel('Wind Direction [deg]')
+        ax.set_ylabel('Energy [kWh]')
+        ax.set_title('Energy per Wind Direction Bin')
+        ax.grid(True)
+        ax.legend()
+
+        return ax
+    
 if __name__ == '__main__':
 
     tab = TableAnalysisBootstrap()
@@ -223,7 +391,7 @@ if __name__ == '__main__':
     })
 
     df_control = df_baseline.copy()
-    df_control['pow_000'] = df_control['pow_000'] * 1.05
+    df_control['pow_000'] = df_control['pow_000'] * 1.25
     df_control['pow_000'] = np.clip(df_control['pow_000'], 0, 1000)
 
     tab.add_df(df_baseline, 'baseline')
@@ -231,6 +399,12 @@ if __name__ == '__main__':
 
     tab.build_bootstrap_tables(n_bootstraps=20, n_blocks=10)
 
-    tab.get_energy_in_range_per_turbine(0)
+    print(tab.get_energy_in_range(turbine_list=[0,1]))
 
-    print(tab.get_energy_in_range_across_turbines())
+    print(tab.get_energy_per_ws_bin(turbine_list=[0,1]).shape)
+
+    tab.plot_energy_per_ws_bin()
+
+    plt.show()
+
+    # print(tab.get_energy_in_range_across_turbines())
