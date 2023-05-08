c36015a71e9d7f63030f10ffef11d841d24fd6b4
Paul <paul.fleming@nrel.gov>
2023-05-08 17:39:59 -0600

    Update pre-calc solution

--- examples_smarteole/01_precalculate_floris_solutions.py
+++ examples_smarteole/01_precalculate_floris_solutions.py
@@ -1,11 +1,11 @@
-import os
+from pathlib import Path
 import numpy as np
 from time import perf_counter as timerpc
 
 from floris.tools import ParallelComputingInterface
 from flasc import floris_tools as ftools
 
-from fct_smarteole.models import load_floris
+from models import load_smarteole_floris
 
 
 if __name__ == "__main__":
@@ -13,17 +13,20 @@ if __name__ == "__main__":
     max_workers = 16
     wake_models = ["jensen", "turbopark", "gch", "cc"]
 
-    root_path = os.path.dirname(os.path.abspath(__file__))
+    # Precalculate FLORIS solutions
+    root_path = Path(__file__).resolve().parent / "precalculated_floris_solutions"
+    root_path.mkdir(exist_ok=True)
+
     for wake_model in wake_models:
-        fn = os.path.join(root_path, "df_fi_approx_{:s}.ftr".format(wake_model))
-        if os.path.exists(fn):
+        fn = root_path / "df_fi_approx_{:s}.ftr".format(wake_model)
+        if fn.is_file():
             print("FLORIS table for '{:s}' model exists. Skipping...".format(wake_model))
             continue
 
         start_time = timerpc()
         print("Precalculating FLORIS table for '{:s}' model...".format(wake_model))
         fi_pci = ParallelComputingInterface(
-            fi=load_floris(wake_model=wake_model),
+            fi=load_smarteole_floris(wake_model=wake_model),
             max_workers=max_workers,
             n_wind_direction_splits=max_workers,
             print_timings=True,
