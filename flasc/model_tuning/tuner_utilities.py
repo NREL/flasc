233fb72dfed1cf6faa38b4d22f36ffa9428756ec
Paul <paul.fleming@nrel.gov>
2023-08-31 13:58:17 -0600

    Add support changing fi parameters within call

--- flasc/model_tuning/tuner_utilities.py
+++ flasc/model_tuning/tuner_utilities.py
@@ -1,52 +1,78 @@
 import numpy as np
 
-from floris.tools import FlorisInterface
-from flasc.examples.models import load_floris_smarteole
+from typing import Any, Dict, List, Optional, Tuple, Union
 
-# Function from 
-# https://stackoverflow.com/questions/14692690/access-nested-dictionary-items-via-a-list-of-keys
-def nested_get(dic, keys):    
+from floris.tools import FlorisInterface
+
+
+
+def nested_get(dic: Dict[str, Any],
+                 keys: List[str]) -> Any:
+    """Get a value from a nested dictionary using a list of keys.
+    Based on: stackoverflow.com/questions/14692690/access-nested-dictionary-items-via-a-list-of-keys
+
+    Args:
+        dic (Dict[str, Any]): The dictionary to get the value from.
+        keys (List[str]): A list of keys to traverse the dictionary.
+
+    Returns:
+        Any: The value at the end of the key traversal.
+    """
     for key in keys:
-        print(key)
-        print(dic)
-        print()
         dic = dic[key]
-        print(dic)
-        print('------------')
     return dic
 
-# Function from 
-# https://stackoverflow.com/questions/14692690/access-nested-dictionary-items-via-a-list-of-keys
-def nested_set(dic, keys, value, idx=None):
+def nested_set(dic: Dict[str, Any], 
+                keys: List[str], 
+                value: Any, 
+                idx: Optional[int] = None) -> None:
+    """Set a value in a nested dictionary using a list of keys.
+    Based on: stackoverflow.com/questions/14692690/access-nested-dictionary-items-via-a-list-of-keys
+
+    Args:
+        dic (Dict[str, Any]): The dictionary to set the value in.
+        keys (List[str]): A list of keys to traverse the dictionary.
+        value (Any): The value to set.
+        idx (Optional[int], optional): If the value is an list, the index to change.
+         Defaults to None.
+    """
     dic_in = dic.copy()
 
     for key in keys[:-1]:
         dic = dic.setdefault(key, {})
     if idx is None:
-        # Parameter is a scaler
+        # Parameter is a scaler, set directly
         dic[keys[-1]] = value
     else:
-        # dic[keys[-1]] = value
-        # Parameter is a list
+        # Parameter is a list, need to first get the list, change the values at idx
 
         # # Get the underlying list
         par_list = nested_get(dic_in, keys)
-        print('x')
-        print(par_list)
-        print('x')
-        # par_list[idx] = value
-        # dic[keys[-1]] = par_list
+        par_list[idx] = value
+        dic[keys[-1]] = par_list
 
-def set_fi_param(fi_in, param, value, idx=None):
 
+def set_fi_param(fi_in: FlorisInterface, 
+        param: List[str], 
+        value: Any, 
+        idx: Optional[int] = None) -> FlorisInterface:
+    """Set a parameter in a FlorisInterface object.
+
+    Args:
+        fi_in (FlorisInterface): The FlorisInterface object to modify.
+        param (List[str]): A list of keys to traverse the FlorisInterface dictionary.
+        value (Any): The value to set.
+        idx (Optional[int], optional): The index to set the value at. Defaults to None.
+
+    Returns:
+        FlorisInterface: The modified FlorisInterface object.
+    """
     fi_dict_mod = fi_in.floris.as_dict()
-
-
     nested_set(fi_dict_mod, param, value, idx)
-
     return FlorisInterface(fi_dict_mod)
 
 
+from flasc.utilities_examples import load_floris_smarteole
 if __name__ == "__main__":
     
     fi, _ = load_floris_smarteole(wake_model="emgauss")
@@ -55,11 +81,11 @@ if __name__ == "__main__":
     param = ['wake','wake_velocity_parameters','empirical_gauss',\
                 'wake_expansion_rates']
     
+
+    fi_2 = set_fi_param(fi, param, 7777777, idx=1)
+
     print(fi_dict_mod)
-
-    fi_2 = set_fi_param(fi, param, 7777777, idx=0)
-
-    print('----')
-
+    print('******')
     print(fi_2.floris.as_dict())
+
 
