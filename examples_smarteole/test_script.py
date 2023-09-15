13d3a91d3b0b956494d08d414d922f962e2fe895
Paul <paul.fleming@nrel.gov>
2023-09-15 16:33:54 -0600

    Update test script

--- examples_smarteole/test_script.py
+++ examples_smarteole/test_script.py
@@ -16,32 +16,26 @@ from flasc.utilities_examples import load_floris_smarteole
 
 warnings.filterwarnings('ignore')
 
-def load_scada(scada_path: str):
-    """
-    Load SCADA
-
-    Args:
-        scada_path (:py:obj:`str`): Path to load SCADA from.
-
-    Returns:
-        df_scada (:py:obj:`pd.DataFrame`): SCADA data.
-    """
-    
-    return pd.read_feather(scada_path)
-
-# Specify SCADA file path and load the dataframe
-scada_path = os.path.join(Path.cwd(), "postprocessed", "df_scada_data_60s_filtered_and_northing_calibrated.ftr")
-df_scada = load_scada(scada_path=scada_path)
 
 # Specify offsets
 start_of_offset = 200 # deg
 end_of_offset = 240 # deg
 
+# Load FLORIS
+fi, _ = load_floris_smarteole(wake_model="emgauss")
+
+# Define D
+D = fi.floris.farm.rotor_diameters[0]
+
+# Specify SCADA file path and load the dataframe
+scada_path = os.path.join(Path.cwd(), "postprocessed", "df_scada_data_60s_filtered_and_northing_calibrated.ftr")
+df_scada = pd.read_feather(scada_path)
+
+
+
 # Limit SCADA to this region
 df_scada = df_scada[(df_scada.wd_smarteole > (start_of_offset - 20)) &
                     (df_scada.wd_smarteole < (end_of_offset + 20))]
-
-print(df_scada.shape)
 
 # Assign wd, ws and pow ref and subset SCADA based on reference variables used in the SMARTEOLE wake steering experiment (TODO reference the experiment)
 df_scada = (df_scada
@@ -52,46 +46,86 @@ df_scada = (df_scada
     )
 )
 
-# Split SCADA into baseline and wake steeering (controlled)
+# Use only the baseline data for the wake recovery tuning
+num_turbines = 7
 df_scada_baseline = df_scada[df_scada.control_mode=='baseline']
-df_scada_controlled = df_scada[df_scada.control_mode=='controlled']
+num_wd_base = df_scada_baseline.shape[0]
+yaw_angles_base = np.zeros([num_wd_base,1,num_turbines])
+yaw_angles_base[:,0, 5] = df_scada_baseline.wind_vane_005.values # Apply angles to SMV6
 
-
-
-fi, _ = load_floris_smarteole(wake_model="emgauss")
-
-# Define D
-D = fi.floris.farm.rotor_diameters[0]
 
 
 # Instantiate a FLORIS model tuner object
 floris_tuner_baseline = FlorisTuner(fi=fi,
-                                    df_scada=df_scada_baseline) 
+                                    df_scada=df_scada_baseline,
+                                    yaw_angles=yaw_angles_base) 
 
 # Specify a range of wake expansion rates (assuming no breakpoints) values
 wake_expansion_rates = np.linspace(start=0.005, 
                                    stop=0.05,
                                    num=20)
 
-# print(wake_expansion_rates)
+# wake_expansion_rates = [0.024]
+
 
 # Determine the optimal value for wake expansion rates
 floris_tuner_baseline.evaluate_parameter_list(param=['wake','wake_velocity_parameters','empirical_gauss','wake_expansion_rates'],
                                           param_values=wake_expansion_rates,
-                                          idx = 0,
+                                          param_idx = 0,
                                           test_turbines=[4],
                                           ref_turbines=[5],
                                           use_predefined_ref=False,
                                           )
 
-# print(floris_tuner_baseline.result_dict[0.01 ].df_result)
 
 print('Computing Error')
 
 floris_tuner_baseline.calculate_param_errors()
-
 floris_tuner_baseline.plot_error()
-
 floris_tuner_baseline.plot_energy_ratios()
 
+
+## WAKE STEEERING
+# Get the FI model with the best parameter applied
+fi_2 = floris_tuner_baseline.apply_best_param()
+
+# Now repeat the tuning on wake steering data
+df_all = df_scada.copy()
+num_wd = df_all.shape[0]
+yaw_angles = np.zeros([num_wd,1,num_turbines])
+yaw_angles[:,0, 5] = df_all.wind_vane_005.values # Apply angles to SMV6
+df_all['df_mode'] = df_all['control_mode'] # df_mode column must exist
+
+
+
+# Use all the data this time and identify the modes of operation for computing uplift
+print('Tuning against wake steering data')
+
+# # Instantiate a FLORIS model tuner object
+floris_tuner_wake_steering = FlorisTuner(fi=fi_2,
+                                    df_scada=df_all,
+                                    yaw_angles=yaw_angles) 
+
+# Specify a range of wake expansion rates (assuming no breakpoints) values
+horizontal_deflection_gains = np.linspace(start=0, 
+                                   stop=4,
+                                   num=10)
+
+floris_tuner_wake_steering.evaluate_parameter_list(param=['wake','wake_deflection_parameters','empirical_gauss','horizontal_deflection_gain_D'],
+                                          param_values=horizontal_deflection_gains,
+                                          test_turbines=[4],
+                                          ref_turbines=[5],
+                                          use_predefined_ref=False,
+                                          compare_uplift=True,
+                                          df_mode_order = ['baseline','controlled']
+                                          )
+
+
+print('Computing Error')
+
+floris_tuner_wake_steering.calculate_param_errors(compare_uplift=True)
+floris_tuner_wake_steering.plot_error()
+floris_tuner_wake_steering.plot_energy_ratio_uplifts()
+
 plt.show()
+
