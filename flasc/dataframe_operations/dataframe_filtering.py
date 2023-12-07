066284d11a568ab9c950966a568275b24cf63ff6
Paul <paul.fleming@nrel.gov>
2023-12-07 15:35:37 -0700

    Ruff format and lint

--- flasc/dataframe_operations/dataframe_filtering.py
+++ flasc/dataframe_operations/dataframe_filtering.py
@@ -11,12 +11,9 @@
 # the License.
 
 
-from matplotlib import pyplot as plt
 import numpy as np
-import pandas as pd
 from scipy.interpolate import interp1d
 
-from .. import utilities as fsut
 from ..dataframe_operations import dataframe_manipulations as dfm
 
 
@@ -38,7 +35,7 @@ def filter_df_by_faulty_impacting_turbines(df, ti, df_impacting_turbines, verbos
         format of:
 
                                0       1          2   3   4   5   6
-                wd                                                 
+                wd
                 0.0       [6, 5]     [5]     [3, 5]  []  []  []  []
                 3.0          [6]     [5]     [3, 5]  []  []  []  []
                 ...          ...     ...        ...  ..  ..  ..  ..
@@ -63,7 +60,8 @@ def filter_df_by_faulty_impacting_turbines(df, ti, df_impacting_turbines, verbos
     # Get number of turbines
     n_turbines = dfm.get_num_turbines(df)
 
-    # Drop all measurements where an upstream turbine is affecting this turbine but also has a NaN measurement itself
+    # Drop all measurements where an upstream turbine is affecting this turbine but also has
+    # a NaN measurement itself
     ws_cols = ["ws_{:03d}".format(ti) for ti in range(n_turbines)]
     pow_cols = ["pow_{:03d}".format(ti) for ti in range(n_turbines)]
 
@@ -80,11 +78,7 @@ def filter_df_by_faulty_impacting_turbines(df, ti, df_impacting_turbines, verbos
         fp = np.hstack([fp, fp[0]])
 
     # Get nearest neighbor indices
-    f = interp1d(
-        x=xp,
-        y=fp,
-        kind="nearest"
-    )
+    f = interp1d(x=xp, y=fp, kind="nearest")
 
     ids = np.array(f(wd_array), dtype=int)
     turbines_impacted = df_impacting_turbines[ti].values[ids]
@@ -94,12 +88,18 @@ def filter_df_by_faulty_impacting_turbines(df, ti, df_impacting_turbines, verbos
     for ii, turbines_impacted_onewd in enumerate(turbines_impacted):
         impacting_turbines_matrix[ii, turbines_impacted_onewd] = True
 
-    # Calculate True/False statement whether any of the turbines shedding a wake on our test_turbine has a NaN ws or pow measurement
-    test_turbine_impacted_by_nan_ws  = np.any(np.isnan(np.array(df[ws_cols], dtype=float))  & impacting_turbines_matrix, axis=1)
-    test_turbine_impacted_by_nan_pow = np.any(np.isnan(np.array(df[pow_cols], dtype=float)) & impacting_turbines_matrix, axis=1)
+    # Calculate True/False statement whether any of the turbines shedding a wake on our test_turbine
+    # has a NaN ws or pow measurement
+    test_turbine_impacted_by_nan_ws = np.any(
+        np.isnan(np.array(df[ws_cols], dtype=float)) & impacting_turbines_matrix, axis=1
+    )
+    test_turbine_impacted_by_nan_pow = np.any(
+        np.isnan(np.array(df[pow_cols], dtype=float)) & impacting_turbines_matrix, axis=1
+    )
     test_turbine_impacted = test_turbine_impacted_by_nan_ws | test_turbine_impacted_by_nan_pow
 
-    # Assign test turbine's measurements to NaN if any turbine that is waking this turbine is reporting NaN measurements
+    # Assign test turbine's measurements to NaN if any turbine that is waking this turbine
+    # is reporting NaN measurements
     N_pre = df_get_no_faulty_measurements(df, ti)
     df_out = df_mark_turbdata_as_faulty(
         df=df,
@@ -110,7 +110,8 @@ def filter_df_by_faulty_impacting_turbines(df, ti, df_impacting_turbines, verbos
 
     if verbose:
         print(
-            "Faulty measurements for WTG {:02d} increased from {:.3f} % to {:.3f} %. Reason: 'Turbine is impacted by faulty upstream turbine'.".format(
+            "Faulty measurements for WTG {:02d} increased from {:.3f} % to {:.3f} %. Reason: "
+            "'Turbine is impacted by faulty upstream turbine'.".format(
                 ti, 100.0 * N_pre / df.shape[0], 100.0 * N_post / df.shape[0]
             )
         )
@@ -121,7 +122,7 @@ def filter_df_by_faulty_impacting_turbines(df, ti, df_impacting_turbines, verbos
 def df_get_no_faulty_measurements(df, turbine):
     if isinstance(turbine, str):
         turbine = int(turbine)
-    entryisnan = np.isnan(df['pow_%03d' % turbine].astype(float))
+    entryisnan = np.isnan(df["pow_%03d" % turbine].astype(float))
     # cols = [s for s in df.columns if s[-4::] == ('_%03d' % turbine)]
     # entryisnan = (np.sum(np.isnan(df[cols]),axis=1) > 0)
     N_isnan = np.sum(entryisnan)
@@ -133,8 +134,7 @@ def df_mark_turbdata_as_faulty(df, cond, turbine_list, exclude_columns=[]):
         turbine_list = [turbine_list]
 
     for ti in turbine_list:
-        cols = [s for s in df.columns if s[-4::] == ('_%03d' % ti)
-                and s not in exclude_columns]
+        cols = [s for s in df.columns if s[-4::] == ("_%03d" % ti) and s not in exclude_columns]
         df.loc[cond, cols] = None  # Delete measurements
 
     return df
