b63b937505c208c1dc70024ca250ca90a17726c7
paulf81 <paul.fleming@nrel.gov>
2024-03-05 12:41:02 -0700

    Convert to BSD license (#176)

--- flasc/data_processing/dataframe_manipulations.py
+++ flasc/data_processing/dataframe_manipulations.py
@@ -1,16 +1,3 @@
-# Copyright 2021 NREL
-
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
-# the License at http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
-
-
 # import datetime
 import datetime
 import os as os
@@ -22,10 +9,7 @@ import numpy as np
 import pandas as pd
 from floris.utilities import wrap_360
 
-from flasc.utilities import (
-    floris_tools as ftools,
-    utilities as fsut,
-)
+from flasc.utilities import floris_tools as ftools, utilities as fsut
 
 
 # Functions related to wind farm analysis for df
