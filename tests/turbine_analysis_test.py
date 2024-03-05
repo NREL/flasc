e5ee6cfd9ba2d933b17eb93139324783300d6662
misi9170 <michael.sinner@nrel.gov>
2024-03-04 21:39:21 -0700

    Ruff format.

--- tests/turbine_analysis_test.py
+++ tests/turbine_analysis_test.py
@@ -6,7 +6,7 @@ from flasc.turbine_analysis.find_sensor_faults import find_sensor_stuck_faults
 
 def test_find_sensor_stuck_faults():
     df_test = pd.DataFrame({"a": [0, 0, 0, 1, 2, 3, 4], "b": [4, 5, 6, 6, 7, 7, 7]})
-    
+
     results_a = find_sensor_stuck_faults(df_test, columns=["a"], ti=0, plot_figures=False)
     assert (results_a == np.array([0, 1, 2])).all()
 
@@ -20,25 +20,17 @@ def test_find_sensor_stuck_faults():
     assert (results_ab == results_ba).all()
 
     results_ab2 = find_sensor_stuck_faults(
-        df_test,
-        columns=["a", "b"],
-        ti=0,
-        n_consecutive_measurements=2,
-        plot_figures=False
+        df_test, columns=["a", "b"], ti=0, n_consecutive_measurements=2, plot_figures=False
     )
     assert (results_ab2 == np.array([0, 1, 2, 3, 4, 5, 6])).all()
 
     df_test = pd.DataFrame({"a": [0, 0.1, -0.1, 0.05, 1]})
     std_true = np.std(df_test["a"][:-1])
-    
+
     results = find_sensor_stuck_faults(df_test, columns=["a"], ti=0, plot_figures=False)
-    assert results.size == 0 # Empty array, no fault detected
+    assert results.size == 0  # Empty array, no fault detected
 
     results = find_sensor_stuck_faults(
-        df_test,
-        columns=["a"],
-        ti=0,
-        stddev_threshold=std_true*2,
-        plot_figures=False
+        df_test, columns=["a"], ti=0, stddev_threshold=std_true * 2, plot_figures=False
     )
     assert (results == np.array([0, 1, 2, 3])).all()
