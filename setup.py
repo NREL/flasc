3cf0cc2123774073b7846550dc79d597ee5fbd97
Paul <paul.fleming@nrel.gov>
2024-05-17 08:20:05 -0600

    fix setup and version

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 """The setup script."""
 
 from pathlib import Path
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
+
 
 # Package meta-data.
 NAME = "flasc"
@@ -12,21 +14,21 @@ AUTHOR = "NREL National Wind Technology Center"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'bokeh~=3.1',
-    'floris~=3.4',
-    'feather-format~=0.0',
-    'ipympl~=0.9',
-    'matplotlib~=3.6',
+    "bokeh~=3.1",
+    "floris~=3.4",
+    "feather-format~=0.0",
+    "ipympl~=0.9",
+    "matplotlib~=3.6",
     "numpy~=1.20",
     "pandas~=2.0",
-    'pyproj~=3.0',
-    'SALib~=1.0',
+    "pyproj~=3.0",
+    "SALib~=1.0",
     "scipy~=1.1",
-    'sqlalchemy~=1.3',
-    'streamlit~=1.0',
-    'tkcalendar~=1.0',
-    'seaborn~=0.0',
-    'polars==0.19.5'
+    "sqlalchemy~=1.3",
+    "streamlit~=1.0",
+    "tkcalendar~=1.0",
+    "seaborn~=0.0",
+    "polars==0.19.5",
 ]
 
 EXTRAS = {
@@ -49,8 +51,9 @@ ROOT = Path(__file__).parent
 with open(ROOT / "flasc" / "version.py") as version_file:
     VERSION = version_file.read().strip()
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     README = readme_file.read()
+
 
 setup_requirements = [
     # Placeholder
@@ -64,30 +67,26 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=README,
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(include=['flasc*']),
-    entry_points={
-        'console_scripts': [
-            'flasc=flasc.cli:main'
-        ]
-    },
+    packages=find_packages(include=["flasc*"]),
+    entry_points={"console_scripts": ["flasc=flasc.cli:main"]},
     include_package_data=True,
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     license="Apache Software License 2.0",
     zip_safe=False,
-    keywords='flasc',
+    keywords="flasc",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     setup_requires=setup_requirements,
 )
