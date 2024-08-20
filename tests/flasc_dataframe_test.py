f31de86f75bef58bafbb5d564439ec267a68f2a9
Paul <paul.fleming@nrel.gov>
2024-08-20 14:13:43 -0600

    Add identity test

--- tests/flasc_dataframe_test.py
+++ tests/flasc_dataframe_test.py
@@ -9,3 +9,15 @@ def test_type():
 
     # Assert df is a pandas DataFrame
     assert isinstance(df, pd.DataFrame)
+
+
+def test_passing_flasc_dataframe_to_flasc_dataframe():
+    df = FlascDataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]}, name_map={"a": "AA"})
+    df2 = FlascDataFrame(df)
+    assert isinstance(df2, FlascDataFrame)
+
+    # Assert df2 is a pandas DataFrame
+    assert isinstance(df2, pd.DataFrame)
+
+    # Assert df and df2 are equal
+    assert df.equals(df2)
