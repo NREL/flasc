67a0de9d1550d84c07bb90aeb3caad7874ce5c09
Paul <paul.fleming@nrel.gov>
2024-09-16 21:38:19 -0600

    Add examples

---
+++ examples_artificial_data/05_model_fit/00_generate_data.py
@@ -0,0 +1,90 @@
+import pickle
+
+import numpy as np
+from floris import TimeSeries
+
+from flasc.model_fit.model_fit import ModelFit
+from flasc.utilities.utilities_examples import load_floris_artificial
+
+""" This example sets up a simple two turbine data set with the Jensen wake model.  Data is saved
+with both uncertain and certain wind speed data."""
+
+# Parameters
+N = 200  # Number of data points
+wd_std = 3.0  # Standard deviation of wind direction in for uncertain model
+we_value_set = 0.03  # Wake expansion value that will be the used to generate the test data
+
+# Get default FLORIS model
+fm_default, _ = load_floris_artificial(wake_model="jensen")
+ufm_default, _ = load_floris_artificial(wake_model="jensen", wd_std=wd_std)
+
+# Set a simple two turbine layout
+layout_x = [0.0, 126.0 * 6.0]
+layout_y = [0.0, 0.0]
+
+# Generate a random series of wind speeds and directions with wind directions
+# focused on turbine 0 waking turbine 1
+np.random.seed(0)
+wind_directions = np.random.uniform(230.0, 310.0, N)
+wind_speeds = np.random.uniform(4.0, 15.0, N)
+
+# wind_directions = np.array([270.0])
+# wind_speeds = np.array([8.0])
+
+time_series = TimeSeries(
+    wind_directions=wind_directions, wind_speeds=wind_speeds, turbulence_intensities=0.06
+)
+
+# Set layout and inflow
+fm_default.set(layout_x=layout_x, layout_y=layout_y, wind_data=time_series)
+ufm_default.set(layout_x=layout_x, layout_y=layout_y, wind_data=time_series)
+
+# Get a new model with a different wake expansion value
+fm_param = fm_default.copy()
+ufm_param = ufm_default.copy()
+
+# Set the FLORIS model parameter
+parameter = ("wake", "wake_velocity_parameters", "jensen", "we")
+we_value_original = fm_param.get_param(parameter)
+fm_param.set_param(parameter, we_value_set)
+ufm_param.set_param(parameter, we_value_set)
+
+# Run
+fm_param.run()
+ufm_param.run()
+
+# Get the turbine powers in kW
+powers = fm_param.get_turbine_powers() / 1000
+powers_u = ufm_param.get_turbine_powers() / 1000
+
+# Make a time column for the flasc_dataframe convention
+time = np.arange(N)
+
+# Build the dataframe
+df = ModelFit.form_flasc_dataframe(
+    time=time, wind_directions=wind_directions, wind_speeds=wind_speeds, powers=powers
+)
+
+df_u = ModelFit.form_flasc_dataframe(
+    time=time, wind_directions=wind_directions, wind_speeds=wind_speeds, powers=powers_u
+)
+
+print(df)
+
+print(df_u)
+
+
+# Save the dataframe and default model and target parameter to a pickle file
+with open("two_turbine_data.pkl", "wb") as f:
+    pickle.dump(
+        {
+            "df": df,
+            "df_u": df_u,
+            "fm_default": fm_default,
+            "ufm_default": ufm_default,
+            "parameter": parameter,
+            "we_value_original": we_value_original,
+            "we_value_set": we_value_set,
+        },
+        f,
+    )
---
+++ examples_artificial_data/05_model_fit/01_evaluate_costs.py
@@ -0,0 +1,64 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from flasc.model_fit.model_fit import ModelFit
+
+# Since ModelFit is always parallel this is important to include
+if __name__ == "__main__":
+    # Load the data from previous example
+    with open("two_turbine_data.pkl", "rb") as f:
+        data = pickle.load(f)
+
+    # Unpack
+    df = data["df"]
+    fm_default = data["fm_default"]
+    parameter = data["parameter"]
+    we_value_original = data["we_value_original"]
+    we_value_set = data["we_value_set"]
+
+    # Define a cost function that checks the RMSE between the power of the second turbine
+    # Note function has to accept fmodel but does not have to use it
+
+    def cost_function(df_scada, df_floris, fmodel):
+        return np.sqrt(np.mean((df_scada["pow_001"].values - df_floris["pow_001"].values) ** 2))
+
+    # Now pass the above cost function to the ModelFit class
+    mf = ModelFit(
+        df,
+        fm_default,
+        cost_function,
+    )
+
+    # Evaluate the model
+    cost_value = mf.evaluate_floris()
+    print(f"Cost value: {cost_value} for model with original parameter value {we_value_original}")
+
+    # Now loop over a range of values for the parameter and evaluate the cost function
+    n_steps = 10
+    param_result = []
+    cost_result = []
+    for i, param_value in enumerate(np.arange(0.01, 0.07, 0.01)):
+        print(f"Evaluating cost function for parameter value {param_value} ({i+1}/{n_steps})")
+        fm_default.set_param(parameter, param_value)
+        mf = ModelFit(
+            df,
+            fm_default,
+            cost_function,
+        )
+        cost_value = mf.evaluate_floris()
+
+        param_result.append(param_value)
+        cost_result.append(cost_value)
+        print(f"... cost value: {cost_value}")
+
+    # Show the results
+    fix, ax = plt.subplots()
+    ax.plot(param_result, cost_result)
+    ax.axvline(we_value_original, color="k", linestyle="--", label="Original value")
+    ax.axvline(we_value_set, color="r", linestyle="--", label="Set value")
+    ax.set_xlabel("Wake expansion value")
+    ax.set_ylabel("Cost value")
+    ax.legend()
+    plt.show()
