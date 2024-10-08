5091d24c6cb79cb97badd29f9911f519b4673a0c
Paul <paul.fleming@nrel.gov>
2024-10-08 14:03:35 -0600

    reorg and add optuna

--- flasc/model_fit/opt_library.py
+++ flasc/model_fit/opt_library.py
@@ -1,18 +1,23 @@
 """This module contains the optimization algorithms for the model fitting."""
 
 import numpy as np
+import optuna
 from floris.utilities import wrap_180
 
 from flasc.model_fit.model_fit import ModelFit
 from flasc.utilities.floris_tools import get_upstream_turbs_floris
 
+## AGGREGATE FUNCTIONS
+## AGGREGATE FUNCTIONS CALL CONDITIONING FUNCTIONS WITHIN A LOOP STRUCTURE
 
-def sweep_across_sectors(
+
+def agg_opt_sectors(
     mf: ModelFit,
     wd_sectors: np.array = np.arange(0, 360, 15.0),
     wind_radius: float = 15.0,
-    n_points: int = 10,
+    atomic_opt: str = "atomic_opt_optuna",
     verbose: bool = False,
+    **kwargs,
 ) -> dict:
     """Loop over wind direction sectors and optimize the model parameters for each.
 
@@ -21,9 +26,10 @@ def sweep_across_sectors(
         wd_sectors (np.array, optional): Array of wind direction sectors to optimize over.
             Defaults to np.arange(0,360,15.).
         wind_radius (float, optional): Radius around wind direction to consider. Defaults to 15.
-        n_points (int, optional): Number of points to evaluate in the parameter space.
-            Defaults to 10.
+        atomic_opt (str, optional): Atomic optimization function to use.
+            Defaults to "atomic_opt_optuna".
         verbose (bool, optional): Whether to print out the optimization process. Defaults to False.
+        kwargs: Additional keyword arguments to pass to the atomic optimization function
 
     Returns:
         dict: Dictionary containing the mean, median, max, min, and std of the parameter values
@@ -37,7 +43,9 @@ def sweep_across_sectors(
 
     for wd_idx, wd in enumerate(wd_sectors):
         print(f"Optimizing for sector {wd_idx+1}/{num_sectors} ({wd} degrees)")
-        res = sweep_sector(mf, wd, wind_radius=wind_radius, n_points=n_points, verbose=verbose)
+        res = opt_sector(
+            mf, wd, wind_radius=wind_radius, atomic_opt=atomic_opt, verbose=verbose, **kwargs
+        )
         res_mat[wd_idx, :] = res["parameter_values"]
 
     # Define some results
@@ -59,12 +67,17 @@ def sweep_across_sectors(
     }
 
 
-def sweep_sector(
+## CONDITIONING FUNCTIONS
+## CONDITIONING FUNCTIONS SET UP AN OPTIMIZATION PROBLEM AND CALL AN ATOMIC FUNCTION
+
+
+def opt_sector(
     mf: ModelFit,
     sector: float,
     wind_radius: float = 15.0,
-    n_points: int = 10,
+    atomic_opt: str = "atomic_opt_optuna",
     verbose: bool = False,
+    **kwargs,
 ) -> dict:
     """Optimize the model parameters for a given wind direction sector.
 
@@ -72,9 +85,10 @@ def sweep_sector(
         mf (ModelFit): ModelFit object
         sector (float): Wind direction sector to optimize over
         wind_radius (float, optional): Radius around wind direction to consider. Defaults to 15.
-        n_points (int, optional): Number of points to evaluate in the parameter space.
-            Defaults to 10.
+        atomic_opt (str, optional): Atomic optimization function to use.
+            Defaults to "atomic_opt_optuna".
         verbose (bool, optional): Whether to print out the optimization process. Defaults to False.
+        kwargs: Additional keyword arguments to pass to the atomic optimization function
 
     Returns:
         dict: Dictionary containing the optimal parameter values, the parameter values tested,
@@ -128,18 +142,39 @@ def sweep_sector(
         mf.parameter_index_list,
     )
 
-    return sweep_opt_sequential(
-        mf_, n_points=n_points, turbine_groupings=turbine_groupings, verbose=verbose
-    )
+    # Now call the atomic optimization function
+    if atomic_opt == "atomic_opt_optuna":
+        # Check if n_trials is in kwargs
+        if "n_trials" in kwargs:
+            n_trials = kwargs["n_trials"]
+        else:
+            n_trials = None
+
+        return atomic_opt_optuna(
+            mf_, n_trials=n_trials, turbine_groupings=turbine_groupings, verbose=verbose
+        )
+
+    elif atomic_opt == "atomic_opt_sweep_sequential":
+        # Check if n_points is in kwargs
+        if "n_points" in kwargs:
+            n_points = kwargs["n_points"]
+        else:
+            n_points = None
+
+        # Pass this through to atomic_opt_sweep_sequential
+        return atomic_opt_sweep_sequential(
+            mf_, n_points=n_points, turbine_groupings=turbine_groupings, verbose=verbose
+        )
 
 
-def sweep_opt_pair(
+def opt_pair(
     mf: ModelFit,
     ref_idx: int,
     test_idx: int,
-    n_points: int = 10,
     wind_radius: float = 15.0,
+    atomic_opt: str = "atomic_opt_optuna",
     verbose: bool = False,
+    **kwargs,
 ) -> dict:
     """Optimize the model parameters for a pair of turbines.
 
@@ -147,12 +182,13 @@ def sweep_opt_pair(
         mf (ModelFit): ModelFit object
         ref_idx (int): Reference turbine index
         test_idx (int): Test turbine index
-        n_points (int, optional): Number of points to evaluate in the parameter space.
-            Defaults to 10.
         wind_radius (float, optional): Radius around wind direction to consider.
             Defaults to 15.
+        atomic_opt (str, optional): Atomic optimization function to use.
+            Defaults to "atomic_opt_optuna".
         verbose (bool, optional): Whether to print out the optimization process.
             Defaults to False.
+        kwargs: Additional keyword arguments to pass to the atomic optimization function
 
     Returns:
         dict: Dictionary containing the optimal parameter values, the parameter values tested,
@@ -194,19 +230,88 @@ def sweep_opt_pair(
     # Set up turbine groupings
     turbine_groupings = {"pow_ref": [ref_idx], "pow_test": [test_idx]}
 
-    # Pass this through to sweep_opt_sequential
-    return sweep_opt_sequential(
-        mf_, n_points=n_points, turbine_groupings=turbine_groupings, verbose=verbose
-    )
+    # Now call the atomic optimization function
+    if atomic_opt == "atomic_opt_optuna":
+        # Check if n_trials is in kwargs
+        if "n_trials" in kwargs:
+            n_trials = kwargs["n_trials"]
+        else:
+            n_trials = None
+
+        return atomic_opt_optuna(
+            mf_, n_trials=n_trials, turbine_groupings=turbine_groupings, verbose=verbose
+        )
+
+    elif atomic_opt == "atomic_opt_sweep_sequential":
+        # Check if n_points is in kwargs
+        if "n_points" in kwargs:
+            n_points = kwargs["n_points"]
+        else:
+            n_points = None
+
+        # Pass this through to atomic_opt_sweep_sequential
+        return atomic_opt_sweep_sequential(
+            mf_, n_points=n_points, turbine_groupings=turbine_groupings, verbose=verbose
+        )
 
 
-# Define an algorithm with works by sweeping through the parameter space
-def sweep_opt_sequential(mf: ModelFit, n_points=10, turbine_groupings=None, verbose=False) -> dict:
+## ATOMIC FUNCTIONS
+## ATOMIC FUNCTIONS SHOULD PERFORM AN OPTIMIZATION AND RETURN A SINGLE RESULT
+
+
+def atomic_opt_optuna(mf: ModelFit, n_trials=None, turbine_groupings=None, verbose=False) -> dict:
+    """Optimize the model parameters using Optuna.
+
+    Args:
+        mf: ModelFit object
+        n_trials: Number of trials to run. Defaults to None (100).
+        turbine_groupings (Dict[str, Tuple], optional): Dictionary of turbine groupings.
+            Defaults to None.
+        verbose: Whether to print out the optimization process. Defaults to False.
+
+    Returns:
+        Dictionary containing the optimal parameter values
+    """
+    if n_trials is None:
+        n_trials = 100
+
+    # Set up the objective function for optuna
+    def objective(trial):
+        parameter_values = []
+        for p_idx in range(mf.n_parameters):
+            parameter_name = mf.parameter_name_list[p_idx]
+            parameter_range = mf.parameter_range_list[p_idx]
+            parameter_values.append(
+                trial.suggest_float(parameter_name, parameter_range[0], parameter_range[1])
+            )
+
+        return mf.set_parameter_and_evaluate(parameter_values, turbine_groupings)
+
+    # Run the optimization
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=n_trials)
+
+    # Make a list of the best parameter values
+    best_params = []
+    for parameter_name in mf.parameter_name_list:
+        best_params.append(study.best_params[parameter_name])
+
+    # Return results as dictionary
+    return {
+        "parameter_values": best_params,
+        "best_cost": study.best_value,
+    }
+
+
+def atomic_opt_sweep_sequential(
+    mf: ModelFit, n_points=None, turbine_groupings=None, verbose=False
+) -> dict:
     """Optimize the model parameters by sweeping through the parameter space.
 
     Args:
         mf: ModelFit object
-        n_points: Number of points to evaluate in the parameter space
+        n_points: Number of points to evaluate in the parameter space.  Defaults to None.  If None,
+            will use the default value of 10.
         turbine_groupings (Dict[str, Tuple], optional): Dictionary of turbine groupings.
             Defaults to None.
         verbose (bool, optional): Whether to print out the optimization process. Defaults to False.
@@ -215,6 +320,9 @@ def sweep_opt_sequential(mf: ModelFit, n_points=10, turbine_groupings=None, verb
         Dictionary containing the optimal parameter values, the parameter values tested,
             and the cost values
     """
+    if n_points is None:
+        n_points = 10
+
     # Start from the initial parameter values
     parameter_values = mf.get_parameter_values()
 
@@ -248,6 +356,11 @@ def sweep_opt_sequential(mf: ModelFit, n_points=10, turbine_groupings=None, verb
         if verbose:
             print(f".Found optimal value for parameter '{parameter_name}': {parameter_values[i]}")
 
+        # Record the optimized cost
+        best_cost = cost_values[optimal_index]
+        if verbose:
+            print(f".best cost: {best_cost}")
+
         # Record the values tests
         parameter_values_sweep_record[parameter_name] = parameter_values_sweep
         cost_values_record[parameter_name] = cost_values
@@ -263,6 +376,7 @@ def sweep_opt_sequential(mf: ModelFit, n_points=10, turbine_groupings=None, verb
     # Return results as dictionary
     return {
         "parameter_values": parameter_values,
+        "best_cost": best_cost,
         "parameter_values_sweep_record": parameter_values_sweep_record,
         "cost_values_record": cost_values_record,
     }
