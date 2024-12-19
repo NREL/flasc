8b0d4d9283db149a0b47f645f46de474daf758b5
Paul <paul.fleming@nrel.gov>
2024-12-18 23:13:36 -0700

    Add shifting plot

--- flasc/analysis/expected_power_analysis_by.py
+++ flasc/analysis/expected_power_analysis_by.py
@@ -218,12 +218,18 @@ class _total_uplift_expected_power_by_:
         self.energy_uplift_lb_pc = energy_uplift_lb_pc
         self.energy_uplift_ub_pc = energy_uplift_ub_pc
 
-    def plot(self, ax=None, color_dict={}) -> Tuple[plt.Figure, plt.Axes]:
+    def plot(
+        self, ax=None, color_dict={}, overwrite_label=None, plot_unc=True
+    ) -> Tuple[plt.Figure, plt.Axes]:
         """Plot the results.
 
         Args:
             ax (plt.Axes): An existing matplotlib Axes object to plot on.  Defaults to None.
             color_dict (Dict): A dictionary of uplift names and colors.  Defaults to {}.
+            overwrite_label (str): If not None, overwrite the uplift names with this label.
+                Defaults to None.
+            plot_unc (bool): If True, plot the uncertainty bounds.  Defaults to True.
+
 
         Returns:
             Tuple[plt.Figure, plt.Axes]: A tuple containing the matplotlib Figure and Axes objects.
@@ -237,6 +243,21 @@ class _total_uplift_expected_power_by_:
             # Get the figure from the ax
             fig = ax.get_figure()
 
+        # If overwrite_labels is not None
+        if overwrite_label is not None:
+            # color_dict can't be empty
+            if len(color_dict) == 0:
+                raise ValueError("color_dict must be provided if overwrite_labels is not None")
+
+            # The entry in color_dict must include overwrite_label
+            if overwrite_label not in color_dict:
+                raise ValueError(f"overwrite_label '{overwrite_label}' not in color")
+
+            # Set uplift_names to be the overwrite_label
+            uplift_names = [overwrite_label]
+        else:
+            uplift_names = self.uplift_names
+
         # If the color dict is empty fill using a matplotlib palette
         if len(color_dict) == 0:
             num_uplift_names = len(self.uplift_names)
@@ -246,7 +267,7 @@ class _total_uplift_expected_power_by_:
             }
 
         # Loop over the uplift names
-        for up_idx, uplift_name in enumerate(self.uplift_names):
+        for up_idx, uplift_name in enumerate(uplift_names):
             # Plot the results
             ax.plot(
                 self.w_bins,
@@ -254,13 +275,14 @@ class _total_uplift_expected_power_by_:
                 label=uplift_name,
                 color=color_dict[uplift_name],
             )
-            ax.fill_between(
-                self.w_bins,
-                self.energy_uplift_lb_pc[up_idx],
-                self.energy_uplift_ub_pc[up_idx],
-                color=color_dict[uplift_name],
-                alpha=0.2,
-            )
+            if plot_unc:
+                ax.fill_between(
+                    self.w_bins,
+                    self.energy_uplift_lb_pc[up_idx],
+                    self.energy_uplift_ub_pc[up_idx],
+                    color=color_dict[uplift_name],
+                    alpha=0.2,
+                )
 
         ax.grid(True)
 
@@ -520,3 +542,109 @@ class total_uplift_expected_power_by_ws(_total_uplift_expected_power_by_):
             remove_any_null_turbine_bins=remove_any_null_turbine_bins,
             cov_terms=cov_terms,
         )
+
+
+def total_uplift_expected_power_by_wd_shift_ws_min(
+    a_in: AnalysisInput,
+    uplift_pairs: List[Tuple[str, str]],
+    uplift_names: List[str],
+    test_turbines: List[int],
+    wd_turbines: List[int] = None,
+    ws_turbines: List[int] = None,
+    use_predefined_wd: bool = False,
+    use_predefined_ws: bool = False,
+    wd_step: float = 2.0,
+    wd_min: float = 0.0,
+    wd_max: float = 360.0,
+    ws_step: float = 1.0,
+    ws_min: float = 0.0,
+    ws_max: float = 50.0,
+    bin_cols_in: List[str] = ["wd_bin", "ws_bin"],
+    weight_by: str = "min",  # min or sum
+    df_freq: pd.DataFrame = None,
+    use_standard_error: bool = True,
+    N: int = 1,
+    percentiles: List[float] = [2.5, 97.5],
+    remove_any_null_turbine_bins: bool = False,
+    cov_terms: str = "zero",
+    n_step: int = 10,
+    ax=None,
+):
+    """Calculate total uplift expected power by wind direction, shifting the wind speed minimum.
+
+    Args:
+        a_in (AnalysisInput): An AnalysisInput object containing the dataframes to analyze
+        uplift_pairs (List[Tuple[str, str]]): A list of tuples containing the df_name
+            values to compare
+        uplift_names (List[str]): A list of names for the uplift results
+        test_turbines (List[int]): A list of turbine indices to test
+        wd_turbines (List[int]): A list of turbine indices for wind direction. Defaults to None.
+        ws_turbines (List[int]): A list of turbine indices for wind speed. Defaults to None.
+        use_predefined_wd (bool): Use predefined wind direction. Defaults to False.
+        use_predefined_ws (bool): Use predefined wind speed. Defaults to False.
+        wd_step (float): The step size for the wind direction bins. Defaults to 2.0.
+        wd_min (float): The minimum wind direction value. Defaults to 0.0.
+        wd_max (float): The maximum wind direction value. Defaults to 360.0.
+        ws_step (float): The step size for the wind speed bins. Defaults to 1.0.
+        ws_min (float): The minimum wind speed value. Defaults to 0.0.
+        ws_max (float): The maximum wind speed value. Defaults to 50.0.
+        bin_cols_in (List[str]): A list of column names to bin the dataframes by.
+            Defaults to ["wd_bin", "ws_bin"].
+        weight_by (str): The method to weight the bins. Defaults to "min".
+        df_freq (pd.DataFrame): A pandas dataframe with the frequency of each bin. Defaults to None.
+        use_standard_error (bool): Use standard error for the uplift calculation. Defaults to True.
+        N (int): The number of bootstrap samples. Defaults to 1.
+        percentiles (List[float]): The percentiles to calculate for the bootstrap samples.
+            Defaults to [2.5, 97.5].
+        remove_any_null_turbine_bins (bool): When computing farm power, remove any bins where
+            and of the test turbines is null.  Defaults to False.
+        cov_terms (str): Use directly computed covariance terms, or fill with zeros or variances.
+            Can be "zero", "var" or "cov".  If "zero" all covariance terms are set to zero.  if
+            "var" all covariance terms are set to the product of the variances.  If "cov" the
+            covariance terms are used as is with missing terms set to product of the
+            variances.  Defaults to "zero".
+        n_step (int): The number of steps to shift the wind speed minimum. Defaults to 10.
+        ax (plt.Axes): An existing matplotlib Axes object to plot on.  Defaults to None.
+    """
+    # Get the min_steps to try
+    ws_min_values = np.linspace(ws_min, ws_min + ws_step, n_step)
+
+    # Define the labels for plotting
+    labels = [f"ws_min = {ws_min_loop:.1f}" for ws_min_loop in ws_min_values]
+
+    # Define the color dict
+    c_pal = plt.cm.get_cmap("viridis", n_step)
+    color_dict = {label: c_pal(idx) for idx, label in enumerate(labels)}
+
+    # Declare a figure
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10.0, 7.0))
+
+    # Loop over these values
+    for ws_min_loop, label in zip(ws_min_values, labels):
+        t_ws = total_uplift_expected_power_by_wd(
+            a_in=a_in,
+            uplift_pairs=uplift_pairs,
+            uplift_names=uplift_names,
+            test_turbines=test_turbines,
+            wd_turbines=wd_turbines,
+            ws_turbines=ws_turbines,
+            use_predefined_wd=use_predefined_wd,
+            use_predefined_ws=use_predefined_ws,
+            wd_step=wd_step,
+            wd_min=wd_min,
+            wd_max=wd_max,
+            ws_step=ws_step,
+            ws_min=ws_min_loop,
+            ws_max=ws_max,
+            bin_cols_in=bin_cols_in,
+            weight_by=weight_by,
+            df_freq=df_freq,
+            use_standard_error=use_standard_error,
+            N=N,
+            percentiles=percentiles,
+            remove_any_null_turbine_bins=remove_any_null_turbine_bins,
+            cov_terms=cov_terms,
+        )
+
+        t_ws.plot(ax=ax, color_dict=color_dict, overwrite_label=label, plot_unc=False)
